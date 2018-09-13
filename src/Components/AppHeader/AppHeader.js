f2aaf15e4967333881ff5e8b8c4299cb24e78313
Miljan Tekic <miljan.tekic@gmail.com>
2018-09-13 14:42:03 +0200

    Created Components folder and setup AppHeader component

---
+++ src/Components/AppHeader/AppHeader.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import {Header} from "../../Library";
+
+class AppHeader extends Component {
+    render() {
+        return (
+            <Header>
+                Tenderly Logo
+            </Header>
+        )
+    }
+}
+
+export default AppHeader;
