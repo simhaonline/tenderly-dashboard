4f8cb60eff059b66798805eb7dd797858fec60f2
Miljan Tekic <miljan.tekic@gmail.com>
2018-09-13 14:42:53 +0200

    Created first page component with dummy text

---
+++ src/Pages/PublicContracts/PublicContractsPage.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+import {Page, Sidebar} from "../../Library";
+
+class PublicContractsPage extends Component {
+    render() {
+        return (
+            <Page>
+                <Sidebar>
+                    testing the sidebar
+                </Sidebar>
+                PublicContractsPage
+            </Page>
+        )
+    }
+}
+
+export default PublicContractsPage;
---
+++ src/Pages/index.js
@@ -0,0 +1,1 @@
+export { default as PublicContractsPage } from './PublicContracts/PublicContractsPage';
