2b06c9de385230299847d56775595c1ab235a9b9
Miljan Tekic <miljan.tekic@gmail.com>
2018-09-13 14:43:38 +0200

    Deleted unused files and changed the index and app files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+
+import {AppHeader} from "./Components";
+import {PublicContractsPage} from "./Pages";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <AppHeader/>
+                <PublicContractsPage/>
+            </div>
+        );
+    }
 }
 
 export default App;
