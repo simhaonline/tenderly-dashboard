31dbe12021c2a31128b602eeafafeef388248b61
Miljan Tekic <miljan.tekic@gmail.com>
2018-09-23 10:01:38 +0200

    Added nav links for public contracts

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -2,13 +2,20 @@ import React, {Component} from 'react';
 
 import './Navigation.css';
 import {Icon} from "../../Elements";
+import {NavLink} from "react-router-dom";
 
 class Navigation extends Component {
     render() {
         return (
             <div className="Navigation">
-                this is main navigation
-                <Icon icon='diamond'/>
+                <NavLink className="HeaderNavItem" exact to="/private">
+                    <Icon icon='project'/>
+                    Dashboard
+                </NavLink>
+                <NavLink className="HeaderNavItem" exact to="/contracts">
+                    <Icon icon='diamond'/>
+                    Public Contracts
+                </NavLink>
             </div>
         );
     }
