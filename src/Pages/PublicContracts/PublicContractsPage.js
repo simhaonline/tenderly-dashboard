9943b1d716e89ddffc1e0c1a15285dd327eeb02c
Miljan Tekic <miljan.tekic@gmail.com>
2018-09-25 17:30:43 +0200

    Connected public contracts page to redux

--- src/Pages/PublicContracts/PublicContractsPage.js
+++ src/Pages/PublicContracts/PublicContractsPage.js
@@ -1,8 +1,18 @@
 import React, {Component} from "react";
+import {bindActionCreators} from "redux";
+import {connect} from "react-redux";
 
+import {NetworkTypes} from "../../Common/constants";
+
+import * as publicContractsActions from '../../Core/PublicContracts/PublicContracts.actions';
 import {Page, Sidebar} from "../../Elements";
 
 class PublicContractsPage extends Component {
+    componentDidMount() {
+        const {actions} = this.props;
+
+        actions.fetchPublicContracts(NetworkTypes.KOVAN, 0);
+    }
     render() {
         return (
             <Page>
@@ -15,4 +25,17 @@ class PublicContractsPage extends Component {
     }
 }
 
-export default PublicContractsPage;
+const mapStateToProps = state => {
+    return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        actions: bindActionCreators(publicContractsActions, dispatch),
+    }
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(PublicContractsPage);
