faac415224c61c97fcbbbf15cb253e48b9cff7d9
Miljan Tekic <miljan.tekic@gmail.com>
2018-09-25 21:00:23 +0200

    Connected public contract page to redux

--- src/Pages/PublicContracts/PublicContractPage.js
+++ src/Pages/PublicContracts/PublicContractPage.js
@@ -1,15 +1,53 @@
 import React, {Component} from "react";
+import {connect} from "react-redux";
 
 import {Page} from "../../Elements";
+import {getPublicContractById} from "../../Common/Selectors/PublicContractSelectors";
+import {bindActionCreators} from "redux";
+import * as publicContractsActions from "../../Core/PublicContracts/PublicContracts.actions";
 
 class PublicContractPage extends Component {
+    componentDidMount() {
+        const {contract, actions, match: {params: { id }}} = this.props;
+
+        if (!contract) {
+            actions.fetchPublicContract(id);
+        }
+    }
     render() {
+        const {contract} = this.props;
+
+        if (!contract) {
+            return (
+                <Page>
+                    Fetching data...
+                </Page>
+            )
+        }
+
         return (
             <Page>
-                Contract Page
+                {contract.name}
             </Page>
         )
     }
 }
 
-export default PublicContractPage;
+const mapStateToProps = (state, ownProps) => {
+    const {match: {params: { id }}} = ownProps;
+
+    return {
+        contract: getPublicContractById(state, id),
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        actions: bindActionCreators(publicContractsActions, dispatch),
+    }
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(PublicContractPage);
