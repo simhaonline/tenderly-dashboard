2be187e4948d4e83798f82f7971263badb532c88
Miljan Tekic <miljan.tekic@gmail.com>
2018-09-25 20:21:02 +0200

    Finished reducer handler for public contracts fetch

--- src/Core/PublicContracts/PublicContracts.actions.js
+++ src/Core/PublicContracts/PublicContracts.actions.js
@@ -9,18 +9,21 @@ export const fetchPublicContracts = (network, page) => {
     return dispatch => {
         const contracts = [
             {
+                id: 'k1',
                 network: 'kovan_testnet',
                 address: '0x0a97094c19295E320D5121d72139A150021a2702',
                 name: 'CryptoMinerToken',
                 compilerVersion: 'v0.4.25',
             },
             {
+                id: 'k2',
                 network: 'kovan_testnet',
                 address: '0x296eaae0c8d9216a46bf6520d37b96058b14d03d',
                 name: 'EnaToken',
                 compilerVersion: 'v0.4.25',
             },
             {
+                id: 'k3',
                 network: 'kovan_testnet',
                 address: '0x7026c4a7cf1fffa16d69efe5239192269203673a',
                 name: 'ClubTransferContract',
--- src/Core/PublicContracts/PublicContracts.reducer.js
+++ src/Core/PublicContracts/PublicContracts.reducer.js
@@ -9,9 +9,26 @@ const initialState = {
 const PublicContractsReducer = (state = initialState, action) => {
     switch (action.type) {
         case FETCH_PUBLIC_CONTRACTS_ACTION:
-            console.log(action);
+            const contracts = action.contracts.reduce((list, contract) => {
+                list[contract.id] = contract;
+                return list;
+            }, {});
+
+            const pageContracts = Object.keys(contracts);
+
             return {
                 ...state,
+                contracts: {
+                    ...state.contracts,
+                    ...contracts,
+                },
+                pages: {
+                    ...state.pages,
+                    [action.network]: {
+                        ...state.pages[action.network],
+                        [action.page]: pageContracts,
+                    }
+                },
             };
         default:
             return state;
