540af99d9ba344c767775f62d8c6a3fcfdddc71c
Miljan Tekic <miljan.tekic@gmail.com>
2018-10-09 20:04:07 +0200

    Added redirect in case the event has not been loaded

--- src/Pages/Error/ErrorPage.js
+++ src/Pages/Error/ErrorPage.js
@@ -1,15 +1,25 @@
 import React, {Component} from "react";
+import {bindActionCreators} from "redux";
+import {connect} from "react-redux";
+import {Redirect} from "react-router-dom";
+
+import * as publicContractsActions from "../../Core/PublicContracts/PublicContracts.actions";
 
 import {Page} from "../../Elements";
 import {NetworkApiToAppTypeMap} from "../../Common/constants";
 import {getPublicContractById, isPublicContractLoaded} from "../../Common/Selectors/PublicContractSelectors";
-import {getPublicContractEvents} from "../../Common/Selectors/EventSelectors";
-import {bindActionCreators} from "redux";
-import * as publicContractsActions from "../../Core/PublicContracts/PublicContracts.actions";
-import connect from "react-redux/es/connect/connect";
+import {getPublicContractEvent} from "../../Common/Selectors/EventSelectors";
 
 class ErrorPage extends Component {
     render() {
+        const {event, contract, redirectNetwork, redirectId} = this.props;
+
+        console.log(event, contract);
+
+        if (!contract) {
+            return <Redirect to={`/contract/${redirectNetwork}/${redirectId}`}/>;
+        }
+
         return (
             <Page>
                 ErrorPage
@@ -19,15 +29,16 @@ class ErrorPage extends Component {
 }
 
 const mapStateToProps = (state, ownProps) => {
-    const {match: {params: { id, network }}} = ownProps;
+    const {match: {params: { id, network, errorId }}} = ownProps;
 
     const networkType = NetworkApiToAppTypeMap[network];
 
     return {
         contract: getPublicContractById(state, id),
         contractLoaded: isPublicContractLoaded(state, id),
-        event: getPublicContractEvents(state, id, networkType),
-        networkType,
+        event: getPublicContractEvent(state, errorId, id, networkType),
+        redirectNetwork: network,
+        redirectId: id,
     };
 };
 
