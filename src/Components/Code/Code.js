876f828df8252613ecf23c04ec306ff78fcff66f
Miljan Tekic <miljan.tekic@gmail.com>
2018-10-19 13:55:34 +0200

    Created code component that can used for solidity contracts

---
+++ src/Components/Code/Code.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import hljs from "highlight.js";
+import hljsDefineSolidity from "highlightjs-solidity";
+
+import './Code.css';
+
+hljsDefineSolidity(hljs);
+hljs.initHighlightingOnLoad();
+
+class Code extends Component {
+    state = {
+        ref: React.createRef(),
+    };
+    componentDidMount() {
+        hljs.highlightBlock(this.state.ref.current);
+    }
+    render() {
+        const {source, line, linePreview} = this.props;
+
+        const lineNumbers = [];
+        const wrapperStyle = {};
+        const codeStyle = {};
+
+        if (line && linePreview) {
+            for (let i = line - linePreview; i <= line + linePreview; i++) {
+                lineNumbers.push({number: i, active: i === line});
+            }
+
+            codeStyle.top = `${(line - linePreview - 1) * - 21}px`;
+            wrapperStyle.height = `${lineNumbers.length * 21}px`;
+        }
+
+        return (
+            <div className="CodeWrapper" style={wrapperStyle}>
+                <div className="StackLines">
+                    {lineNumbers.map(num =>
+                        <div key={num.number} className={`StackLine ${num.active? 'active': ''}`}>{num.number}</div>
+                    )}
+                </div>
+                <pre className="StackCode" ref={this.state.ref} style={codeStyle}>
+                    {source}
+                </pre>
+            </div>
+        )
+    }
+}
+
+export default Code;
