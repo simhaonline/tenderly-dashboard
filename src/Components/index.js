208f5e64d54f5ce8bfa06dff33771da20731a618
Miljan Tekic <miljan.tekic@gmail.com>
2018-10-19 14:53:19 +0200

    Exposed components

--- src/Components/index.js
+++ src/Components/index.js
@@ -3,6 +3,8 @@ export {default as Navigation} from './Navigation/Navigation';
 export {default as PrivateRoute} from './PrivateRoute/PrivateRoute';
 export {default as EtherscanLink} from './EtherscanLink/EtherscanLink';
 export {default as EventList} from './EventList/EventList';
+export {default as ContractSource} from './ContractSource/ContractSource';
+export {default as Code} from './Code/Code';
 export {default as PublicContractList} from './PublicContractList/PublicContractList';
 export {default as PublicContractsSwitcher} from './PublicContractsSwitcher/PublicContractsSwitcher';
 export {default as PublicContractsInfo} from './PublicContractsInfo/PublicContractsInfo';
