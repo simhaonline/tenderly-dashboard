2287e168286f8499b91158eee51e11cb31becd94
Miljan Tekic <miljan.tekic@gmail.com>
2018-10-20 19:25:10 +0200

    Added kovan info to public contracts page

--- src/Components/PublicContractsInfo/PublicContractsInfo.js
+++ src/Components/PublicContractsInfo/PublicContractsInfo.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import EtherscanLink from "../EtherscanLink/EtherscanLink";
+import NetworkTag from "../NetworkTag/NetworkTag";
 
 import './PublicContractsInfo.css';
 
 const PublicContractsInfo = ({network}) => {
     return (
         <div className="PublicContractsInfo">
-            <h2>Public Contracts</h2>
+            <h2>Public Contracts <NetworkTag network={network}/></h2>
             <p>We track all contract errors with a full stack trace that happen on public <EtherscanLink network={network} path={"contractsVerified"}>verified contracts</EtherscanLink>.</p>
         </div>
     )
