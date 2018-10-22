d11cee07a04b016cdc990c75830b665e115f415a
Miljan Tekic <miljan.tekic@gmail.com>
2018-10-22 18:15:52 +0200

    Connected listing of projects

--- src/Components/DashboardProjectsList/DashboardProjectsList.js
+++ src/Components/DashboardProjectsList/DashboardProjectsList.js
@@ -4,15 +4,26 @@ import {Button} from "../../Elements";
 
 import './DashboardProjectsList.css';
 
-const DashboardProjectsList = ({projects}) => {
+const DashboardProjectsList = ({projects, loaded}) => {
     return (
         <div className="DashboardProjectsList">
             <h2>Projects</h2>
             <hr/>
-            {(!projects || projects.length === 0) && <div className="ProjectListEmptyState">
+            {(loaded && projects.length === 0) && <div className="ProjectListEmptyState">
                 <h5>No project created yet!</h5>
                 <p>Create a project and upload your Smart Contract to start tracking them</p>
                 <Button to="/project/create">Create Project</Button>
+            </div>}
+            {(loaded && projects.length !== 0) && <div className="ProjectList">
+                {projects.map(project =>
+                    <div className="ProjectListItem" key={project.id}>
+                        {project.name}
+                    </div>
+                )}
+                <Button to="/project/create">Create Project</Button>
+            </div>}
+            {!loaded && <div className="LoadingProjects">
+                Loading...
             </div>}
         </div>
     )
