d84570f9638dec16725f1d7737ddd39959bfb22a
Miljan Tekic <miljan.tekic@gmail.com>
2018-11-06 18:34:13 +0100

    Implemented contract list on project page

--- src/Pages/Project/ProjectContractsPage.js
+++ src/Pages/Project/ProjectContractsPage.js
@@ -1,15 +1,33 @@
 import React, {Component} from 'react';
+import {bindActionCreators} from "redux";
 import {connect} from "react-redux";
 
-import {Container, Page} from "../../Elements";
+import * as contractActions from "../../Core/Contract/Contract.actions";
+
 import {getProject} from "../../Common/Selectors/ProjectSelectors";
 
+import {Container, Page} from "../../Elements";
+import ProjectContractList from "../../Components/ProjectContractList/ProjectContractList";
+
 class ProjectContractsPage extends Component {
+    async componentDidMount() {
+        const {actions, project, contractsLoaded} = this.props;
+
+        if (!contractsLoaded) {
+            await actions.fetchContractsForProject(project.id);
+        }
+    }
+
     render() {
+        const {contracts, contractsLoaded} = this.props;
+
         return (
             <Page id="ProjectPage">
                 <Container>
-                    ProjectContractsPage
+                    {(contractsLoaded && contracts.length) && <ProjectContractList contracts={contracts}/>}
+                    {!contractsLoaded && <div>
+                        Loading...
+                    </div>}
                 </Container>
             </Page>
         )
@@ -21,10 +39,18 @@ const mapStateToProps = (state, ownProps) => {
 
     return {
         project: getProject(state, id),
+        contracts: [],
+        contractsLoaded: false,
+    }
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        actions: bindActionCreators(contractActions, dispatch),
     }
 };
 
 export default connect(
     mapStateToProps,
-    null
+    mapDispatchToProps,
 )(ProjectContractsPage);
