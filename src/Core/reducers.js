ddabb7c3dc18e10ecea53ae6f965efb528295612
Miljan Tekic <miljan.tekic@gmail.com>
2018-11-06 18:46:21 +0100

    Registered contract reducer

--- src/Core/reducers.js
+++ src/Core/reducers.js
@@ -4,12 +4,14 @@ import AuthReducer from "./Auth/Auth.reducer";
 import PublicContractsReducer from "./PublicContracts/PublicContracts.reducer";
 import EventReducer from "./Event/Event.reducer";
 import ProjectReducer from "./Project/Project.reducer";
+import ContractReducer from "./Contract/Contract.reducer";
 
 const reducers = combineReducers({
     auth: AuthReducer,
     publicContracts: PublicContractsReducer,
     event: EventReducer,
     project: ProjectReducer,
+    contract: ContractReducer,
 });
 
 export default reducers;
