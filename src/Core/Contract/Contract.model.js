7b49a2d2a790daa0bfc614de12f030d422abffd8
Miljan Tekic <miljan.tekic@gmail.com>
2018-11-07 11:05:28 +0100

    Updated Contract model

--- src/Core/Contract/Contract.model.js
+++ src/Core/Contract/Contract.model.js
@@ -1,5 +1,63 @@
+import {NetworkApiToAppTypeMap} from "../../Common/constants";
+
 class Contract {
     constructor(data) {
-        console.log(data);
+        /** @type string */
+        this.id = data.deployment_information.address;
+
+        /** @type string */
+        this.name = data.contract_name;
+
+        /** @type string */
+        this.address = data.deployment_information.address;
+
+        /** @type string */
+        this.network = NetworkApiToAppTypeMap[data.deployment_information.network_id];
+
+        /** @type Date */
+        this.lastEventAt = data.last_event_occurred_at;
+
+        /** @type number */
+        this.eventCount = data.number_of_exceptions;
+
+        if (data.source) {
+            /** @type string */
+            this.source = data.source;
+
+            /** @type string */
+            this.solidity = Contract.getSolidityVersion(data.source);
+        }
+    }
+
+    /**
+     * @returns {string}
+     */
+    getFileName() {
+        return `${this.name}.sol`;
+    }
+
+    /**
+     * @returns {Contract}
+     */
+    update() {
+        return this;
+    }
+
+    /**
+     * @param {string} source
+     * @returns {string|null}
+     */
+    static getSolidityVersion(source) {
+        if (!source) {
+            return null;
+        }
+
+        const versionRegex = /solidity \^(.*);/g;
+
+        const matches = versionRegex.exec(source);
+
+        return matches[1];
     }
 }
+
+export default Contract;
