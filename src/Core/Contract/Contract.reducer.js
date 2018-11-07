303532869c872a2dd05ec27ada659186077b3090
Miljan Tekic <miljan.tekic@gmail.com>
2018-11-07 11:13:34 +0100

    Reducer logic for fetching project contracts

--- src/Core/Contract/Contract.reducer.js
+++ src/Core/Contract/Contract.reducer.js
@@ -1,3 +1,7 @@
+import {FETCH_CONTRACTS_FOR_PROJECT_ACTION} from "./Contract.actions";
+
+import {EntityStatusTypes} from "../../Common/constants";
+
 const initialState = {
     contracts: {},
     contractStatus: {},
@@ -5,6 +9,35 @@ const initialState = {
 
 const ContractReducer = (state = initialState, action) => {
     switch (action.type) {
+        case FETCH_CONTRACTS_FOR_PROJECT_ACTION:
+            const computedData = action.contracts.reduce((data, contract) => {
+
+                if (state.contracts[contract.id]) {
+                    const existingContract = state.contracts[contract.id];
+
+                    data.contracts[contract.id] = existingContract.update(contract);
+                } else {
+                    data.contracts[contract.id] = contract;
+                    data.contractStatus[contract.id] = EntityStatusTypes.LOADED;
+                }
+
+                return data;
+            }, {
+                contracts: {},
+                contractStatus: {},
+            });
+
+            return {
+                ...state,
+                contracts: {
+                    ...state.contracts,
+                    ...computedData.contracts,
+                },
+                contractStatus: {
+                    ...state.contractStatus,
+                    ...computedData.contractStatus,
+                },
+            };
         default:
             return state;
     }
