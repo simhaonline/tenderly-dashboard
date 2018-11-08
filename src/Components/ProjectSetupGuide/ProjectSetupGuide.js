8b10565976fc16b64c997338467e7c2ae13b0846
Miljan Tekic <miljan.tekic@gmail.com>
2018-11-08 11:56:05 +0100

    Removed guide from card and moved styles

--- src/Components/ProjectSetupGuide/ProjectSetupGuide.js
+++ src/Components/ProjectSetupGuide/ProjectSetupGuide.js
@@ -42,26 +42,6 @@ class ProjectSetupGuide extends Component {
                 <Card className="ProjectSetupGuide">
                     <h2>Project setup</h2>
                     <div className="ProjectSetupWrapper">
-                        <p>In order to monitor your contracts you need to install tenderly cli and have truffle configured</p>
-                        <p>To install tenderly</p>
-                        <p>On macOS</p>
-                        <code>
-                            brew tap tenderly/tenderly
-                            <br/>
-                            brew install tenderly
-                        </code>
-                        <p>To login into your tenderly account</p>
-                        <code>
-                            tenderly login
-                        </code>
-                        <p>In your project folder where truffle is configured run</p>
-                        <code>
-                            tenderly init
-                        </code>
-                        <p>Start monitoring your contracts by doing</p>
-                        <code>
-                            tenderly push
-                        </code>
                         <Button outline onClick={this.openDialog}>
                             <span>Setup Project</span>
                         </Button>
