0a35948e8a7f480d2c942be46c4233ab4a6f9eb9
Miljan Tekic <miljan.tekic@gmail.com>
2018-11-08 11:05:31 +0100

    Added additional dialog props

--- src/Elements/Dialog/Dialog.js
+++ src/Elements/Dialog/Dialog.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import Modal from 'react-modal';
+import classNames from 'classnames';
 
 import './Dialog.css';
 
 Modal.setAppElement('#root');
 
-const Dialog = ({open = false, onClose, children, overlayClose = true, escapeClose = true}) => {
+const Dialog = ({open = false, onClose, children, overlayClose = true, escapeClose = true, className}) => {
     return (
         <Modal isOpen={open}
                onRequestClose={onClose}
                closeTimeoutMS={200}
                overlayClassName="DialogOverlay"
-               className="DialogContent"
+               className={classNames(
+                   "DialogContent",
+                   className,
+               )}
                bodyOpenClassName="DialogOpened"
                shouldCloseOnOverlayClick={overlayClose}
                shouldCloseOnEsc={escapeClose}>
