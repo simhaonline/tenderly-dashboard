bdaf8410f9ad48d119877a8d607cefbb0777542e
Miljan Tekic <miljan.tekic@gmail.com>
2018-11-09 15:04:43 +0100

    Updated the project list

--- src/Components/DashboardProjectsList/DashboardProjectsList.js
+++ src/Components/DashboardProjectsList/DashboardProjectsList.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import moment from "moment";
+import {Bar, BarChart, YAxis} from "recharts";
 
-import {Button} from "../../Elements";
+import {Button, Icon} from "../../Elements";
 import ProjectSetupGuide from "../ProjectSetupGuide/ProjectSetupGuide";
 
 import './DashboardProjectsList.css';
 
 const DashboardProjectsList = ({projects, loaded}) => {
+    const data = [
+        {name: 'Page A', events: 0.1,},
+        {name: 'Page B', events: 0.1,},
+        {name: 'Page C', events: 0.1,},
+        {name: 'Page D', events: 0.1,},
+        {name: 'Page E', events: 0.1,},
+        {name: 'Page F', events: 0.1,},
+        {name: 'Page G', events: 0.1,},
+        {name: 'Page G', events: 0.1,},
+        {name: 'Page G', events: 0.1,},
+        {name: 'Page G', events: 0.1,},
+        {name: 'Page G', events: 0.1,},
+        {name: 'Page G', events: 0.1,},
+        {name: 'Page G', events: 0.1,},
+        {name: 'Page G', events: 0.1,},
+    ];
+
     return (
         <div className="DashboardProjectsList">
             <h2>Projects</h2>
@@ -20,11 +38,23 @@ const DashboardProjectsList = ({projects, loaded}) => {
             {(loaded && projects.length !== 0) && <div className="ProjectList">
                 {projects.map(project =>
                     <Link to={`/project/${project.id}`} className="ProjectListItem" key={project.id}>
-                        <div className="ProjectNameLabel">Project name:</div>
-                        <div className="ProjectName">{project.name}</div>
+                        <div className="ProjectNameWrapper">
+                            <div className="ProjectName">{project.name}</div>
+                            <div className="ProjectId">{project.id}</div>
+                        </div>
+                        <div className="ProjectChartWrapper">
+                            {!!project.lastPushAt && <BarChart width={220} height={80} data={data}>
+                                <YAxis type="number" domain={[0,  dataMax => Math.max(dataMax, 8)]} hide/>
+                                <Bar dataKey='events' fill='#df0074'/>
+                            </BarChart>}
+                            {!project.lastPushAt && <div className="ProjectNotSetupState">
+                                <Icon icon="cloud-off"/>
+                                <span>Not contract pushed yet</span>
+                            </div>}
+                        </div>
                         <div className="ProjectContractInfo">
-                            {!!project.lastPushAt && <div>
-                                Last contract deployed at: {moment(project.lastPushAt).format("MMM DD YYYY, HH:mm:ss")}
+                            {!!project.lastPushAt && <div className="LastDeployInfo">
+                                Last deployment: <span>{moment(project.lastPushAt).fromNow()}</span>
                             </div>}
                             {!project.lastPushAt && <div onClick={event => event.preventDefault()}>
                                 <ProjectSetupGuide label="Push Contracts" projectId={project.id} size="extra-small" color="secondary"/>
