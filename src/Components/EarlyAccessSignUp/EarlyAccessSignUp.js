99c3db339441883514e7cb5c5fb0bd95dfebaa7f
Miljan Tekic <miljan.tekic@gmail.com>
2018-11-09 12:24:23 +0100

    Eslint ignore for regex

--- src/Components/EarlyAccessSignUp/EarlyAccessSignUp.js
+++ src/Components/EarlyAccessSignUp/EarlyAccessSignUp.js
@@ -27,6 +27,7 @@ class EarlyAccessSignUp extends Component {
     }
 
     validateEmail = (email) => {
+        // eslint-disable-next-line
         const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return emailRegex.test(String(email).toLowerCase());
     };
