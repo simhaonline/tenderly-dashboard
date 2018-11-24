8a53c5d4568a582a660b26e4f5cc018de047dd87
Miljan Tekic <miljan.tekic@gmail.com>
2018-11-24 13:22:44 +0100

    Connected private project contracts to redux

--- src/Pages/Project/ProjectContractPage.js
+++ src/Pages/Project/ProjectContractPage.js
@@ -1,23 +1,64 @@
-import React, {Component} from "react";
-import {bindActionCreators} from "redux";
+import React, {Component, Fragment} from "react";
 import {connect} from "react-redux";
 import {Redirect} from "react-router-dom";
 
+import {getContractById, getContractStatus} from "../../Common/Selectors/ContractSelectors";
+
 import {Page, Container} from "../../Elements";
+import {ContractInformation} from "../../Components";
+import {EntityStatusTypes} from "../../Common/constants";
 
 class ProjectContractPage extends Component {
+    componentDidMount() {
+        const {contractStatus} = this.props;
+
+        if (contractStatus === EntityStatusTypes.NOT_LOADED) {
+
+        }
+    }
+
+    /**
+     * @return {boolean}
+     */
+    isContractLoaded = () => {
+        const {contractStatus} = this.props;
+
+        return contractStatus === EntityStatusTypes.LOADED;
+    };
+
     render() {
+        const {contract, contractStatus, projectId} = this.props;
+
+        if (contractStatus === EntityStatusTypes.NON_EXISTING) {
+            return <Redirect to={`/project/${projectId}/contracts`}/>
+        }
+
+        const isContractFetched = this.isContractLoaded();
+
         return (
             <Page>
                 <Container>
-                    ProjectContractPage
+                    {!isContractFetched && <div>Loading...</div>}
+                    {isContractFetched && <Fragment>
+                        <ContractInformation contract={contract}/>
+                    </Fragment>}
                 </Container>
             </Page>
         );
     }
 }
 
+const mapStateToProps = (state, ownProps) => {
+    const {match: {params: {id, contractId}}} = ownProps;
+
+    return {
+        projectId: id,
+        contract: getContractById(state, contractId),
+        contractStatus: getContractStatus(state, contractId),
+    }
+};
+
 export default connect(
-    null,
+    mapStateToProps,
     null,
 )(ProjectContractPage);
