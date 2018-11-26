2aba0c93b3550a9b116aaf6b66c3153b427984ec
Miljan Tekic <miljan.tekic@gmail.com>
2018-11-26 09:29:08 +0100

    Created feature flag for internal features

---
+++ src/Components/FeatureFlag/FeatureFlag.js
@@ -0,0 +1,26 @@
+import {Component} from 'react';
+import {connect} from "react-redux";
+
+class FeatureFlag extends Component {
+    render() {
+        const {user, children} = this.props;
+
+
+        if (!user.email.includes('@tenderly.app')) {
+            return null;
+        }
+
+        return children;
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        user: state.auth.user,
+    }
+};
+
+export default connect(
+    mapStateToProps,
+    null,
+)(FeatureFlag);
