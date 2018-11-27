c4da526c2f15d534cf50d47a17dabc03c983977a
Miljan Tekic <miljan.tekic@gmail.com>
2018-11-27 15:11:15 +0100

    Tidied up the sign up info

--- src/Pages/Public/LoginPage.js
+++ src/Pages/Public/LoginPage.js
@@ -81,10 +81,10 @@ class LoginPage extends Component {
                             </FeatureFlag>
                         </Form>
                         <div className="SignUpWrapper">
-                            <h2>Join our Private Alpha Program</h2>
-                            <p>Join our limited private alpha program where we will be releasing updates to a small number of users as we develop them.</p>
-                            <p>You can see a full list of <a href="https://tenderly.app#features">our features here</a>.</p>
-                            <EarlyAccessButton label="Sign up"/>
+                            <h2 className="SignUpHeading">Sign up for our Alpha Program!</h2>
+                            <p className="SignUpText">Join our private alpha program where we will be releasing updates to a small number of users as we develop them.</p>
+                            <p className="SignUpText">You can see a full list of <a href="https://tenderly.app#features">features here</a> that we plan to develop for you.</p>
+                            <EarlyAccessButton label="Sign up for Alpha"/>
                         </div>
                     </div>
                     <div className="DocumentsWrapper">
