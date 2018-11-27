f0afde516a4a7cdd8bb664dc6fb5e1a794f15443
Miljan Tekic <miljan.tekic@gmail.com>
2018-11-27 09:10:44 +0100

    Label and icon for project form

--- src/Components/CreateProjectForm/CreateProjectForm.js
+++ src/Components/CreateProjectForm/CreateProjectForm.js
@@ -5,7 +5,7 @@ import {Form, Input, Button} from "../../Elements";
 const CreateProjectForm = ({formData = {}, onChange, onSubmit}) => {
     return (
         <Form onSubmit={onSubmit}>
-            <Input value={formData.projectName} field={"projectName"} onChange={onChange}/>
+            <Input value={formData.projectName} label="Project name" icon="project" field={"projectName"} onChange={onChange}/>
             <Button type="submit">
                 <span>Create</span>
             </Button>
