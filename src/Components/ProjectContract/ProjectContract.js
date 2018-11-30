7c10cfd10d0df292b830f3298c6efe6b9969decc
Miljan Tekic <miljan.tekic@gmail.com>
2018-11-30 08:23:35 +0100

    Finished styling project contract

--- src/Components/ProjectContract/ProjectContract.js
+++ src/Components/ProjectContract/ProjectContract.js
@@ -1,33 +1,58 @@
 import React from 'react';
 import moment from "moment";
 
+import {EtherscanLinkTypes} from "../../Common/constants";
+
+import {Card, Button} from "../../Elements";
 import NetworkTag from "../NetworkTag/NetworkTag";
 import PageLink from "../PageLink/PageLink";
+import EtherscanLink from "../EtherscanLink/EtherscanLink";
 
 import './ProjectContract.css';
 
 const ProjectContract = ({contract}) => {
+    const hasEvents = !!contract.eventCount;
     return (
         <PageLink className="ProjectContract" to={`/project/${contract.projectId}/contract/${contract.id}`}>
-            <h3>{contract.name}</h3>
-            <div>{contract.getFileName()}</div>
-            <div>
-                <h5>Deployment Info:</h5>
-                {contract.lastDeploymentAt && <div>Last deployment: {moment(contract.lastDeploymentAt).format("MMM DD YYYY, HH:mm:ss")}</div>}
-                <div>Total deployments: {contract.deploymentCount}</div>
-                <div>Current address: {contract.address}</div>
-            </div>
-            <div>
-                <h5>Events Info:</h5>
-                {contract.lastEventAt && <div>Last event: {moment(contract.lastEventAt).format("MMM DD YYYY, HH:mm:ss")}</div>}
-                <div>Total events: {contract.eventCount}</div>
-            </div>
-            <div>
-                <h5>Networks:</h5>
-                <div>
-                    <NetworkTag network={contract.network}/>
+            <Card>
+                <div className="ContentWrapper">
+                    <NetworkTag network={contract.network} size="small" className="ContractNetworkTag"/>
+                    <div className="GeneralInfoColumn ItemColumn">
+                        <h3 className="ContractName">{contract.name}</h3>
+                        <EtherscanLink onClick={event => event.stopPropagation()}
+                                       className="ContractAddress"
+                                       network={contract.network}
+                                       type={EtherscanLinkTypes.ADDRESS}
+                                       value={contract.address}>
+                            {contract.address}
+                        </EtherscanLink>
+                    </div>
+                    <div className="EventsColumn ItemColumn">
+                        <div className="InfoLabel">No. of Events:</div>
+                        {hasEvents && <div>{contract.eventCount}</div>}
+                        {!hasEvents && <div>/</div>}
+                    </div>
+                    {contract.lastEventAt && <div className="LastEventColumn ItemColumn">
+                        <div className="InfoLabel">Last event:</div>
+                        <div>{moment(contract.lastEventAt).fromNow()}</div>
+                    </div>}
+                    {contract.lastDeploymentAt && <div className="DeploymentInfoColumn ItemColumn">
+                        <div className="InfoLabel">Deployed:</div>
+                        <div title={moment(contract.lastDeploymentAt).format("MMM DD YYYY, HH:mm:ss")}>{moment(contract.lastDeploymentAt).fromNow()}</div>
+                    </div>}
+                    <div className="ActionsColumn ItemColumn">
+                        {hasEvents && <Button size="small" outline color="secondary" onClick={event => event.stopPropagation()}
+                                to={`/project/${contract.projectId}/events?contract=${contract.id}`} className="Action">View
+                            <span>Events</span>
+                        </Button>}
+                        <Button size="small" outline color="secondary" onClick={event => event.stopPropagation()}
+                                to={`/project/${contract.projectId}/contract/${contract.id}/source`}
+                                className="Action">
+                            <span>Source</span>
+                        </Button>
+                    </div>
                 </div>
-            </div>
+            </Card>
         </PageLink>
     )
 };
