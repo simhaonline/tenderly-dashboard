6dbd8d17b67fcd1cff29798d04a6d75e577b15b9
Miljan Tekic <miljan.tekic@gmail.com>
2018-12-03 16:50:57 +0100

    Removed unused imports

--- src/Pages/Project/CreateProjectPage.js
+++ src/Pages/Project/CreateProjectPage.js
@@ -3,7 +3,6 @@ import {bindActionCreators} from "redux";
 import {connect} from "react-redux";
 import {Redirect} from "react-router-dom";
 
-import {initializeForm, updateFormField} from "../../Utils/FormHelpers";
 import * as projectActions from '../../Core/Project/Project.actions';
 
 import {Page, Container} from "../../Elements";
