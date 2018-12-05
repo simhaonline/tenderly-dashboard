3fbce99147b6e4a049a94e94be75360fb06b2ae1
Miljan Tekic <miljan.tekic@gmail.com>
2018-12-05 21:18:59 +0100

    Wrapped MixPanel events to not be fired in development

--- src/Utils/MixPanel.js
+++ src/Utils/MixPanel.js
@@ -6,20 +6,24 @@ const _mixpanel = mixpanel;
 
 class MixPanel {
     static initialize() {
-        _mixpanel.init(mixpanelToken);
+        if (process.env.NODE_ENV !== 'development') {
+            _mixpanel.init(mixpanelToken);
+        }
     }
 
     /**
      * @param {User} user
      */
     static setUser(user) {
-        _mixpanel.identify(user.id);
-        _mixpanel.people.set({
-            "$email": user.email,
-            "$username": user.username,
-            "$first_name": user.firstName,
-            "$last_name": user.lastName,
-        });
+        if (process.env.NODE_ENV !== 'development') {
+            _mixpanel.identify(user.id);
+            _mixpanel.people.set({
+                "$email": user.email,
+                "$username": user.username,
+                "$first_name": user.firstName,
+                "$last_name": user.lastName,
+            });
+        }
     }
 
     /**
@@ -27,7 +31,9 @@ class MixPanel {
      * @param {Object} [data]
      */
     static track(event, data = {}) {
-        _mixpanel.track(event, data)
+        if (process.env.NODE_ENV !== 'development') {
+            _mixpanel.track(event, data)
+        }
     }
 }
 
