d6ff8e5ad3ddeef14f97a6911849bb7bffe1ba4b
Miljan Tekic <miljan.tekic@gmail.com>
2018-12-06 09:07:30 +0100

    Added MixPanel events

--- src/Components/ProjectEventActions/ProjectEventActions.js
+++ src/Components/ProjectEventActions/ProjectEventActions.js
@@ -1,19 +1,45 @@
 import React, {Component} from 'react';
 
+import MixPanel from "../../Utils/MixPanel";
+
 import {Button, Icon} from "../../Elements";
 
 class ProjectEventActions extends Component {
+    handlePreviousEventsPage = () => {
+        const {page} = this.props;
+
+        MixPanel.track('Project Events - Previous Page', {
+            currentPage: page,
+        });
+    };
+
+    handleNextEventsPage = () => {
+        const {page} = this.props;
+
+        MixPanel.track('Project Events - Next Page', {
+            currentPage: page,
+        });
+    };
+
     render() {
+        const {projectId, page} = this.props;
+
         return (
             <div className="ProjectEventActions">
                 <Button outline size="small">
                     <Icon icon="refresh-cw"/>
                 </Button>
                 <div className="PaginationControls">
-                    <Button outline size="small">
+                    <Button outline size="small" to={{
+                        pathname: `/project/${projectId}/events`,
+                        search: `?page=${page - 1}`
+                    }} onClick={this.handlePreviousEventsPage}>
                         <Icon icon="arrow-left"/>
                     </Button>
-                    <Button outline size="small">
+                    <Button outline size="small" to={{
+                        pathname: `/project/${projectId}/events`,
+                        search: `?page=${page + 1}`
+                    }} onClick={this.handleNextEventsPage}>
                         <Icon icon="arrow-right"/>
                     </Button>
                 </div>
