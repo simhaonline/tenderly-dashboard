f01977e52b0003fc25a407bbdc8b2ebf12d4ed8e
Miljan Tekic <miljan.tekic@gmail.com>
2018-12-07 09:28:37 +0100

    Created PaginationControls component

---
+++ src/Components/PaginationControls/PaginationControls.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import {Button, Icon} from "../../Elements";
+
+import './PaginationControls.css';
+
+const PaginationControls = ({onPrevious, onNext, current, max, disabled}) => {
+    return (
+        <div className="PaginationControls">
+            <Button outline size="small" className="PaginationPreviousControl" onClick={onPrevious} disabled={current <= 0 || disabled}>
+                <Icon icon="chevron-left"/>
+            </Button>
+            <div className="CurrentPagination">
+                <span>{current + 1}</span>
+            </div>
+            <Button outline size="small" className="PaginationNextControl" onClick={onNext} disabled={current >= max || disabled}>
+                <Icon icon="chevron-right"/>
+            </Button>
+        </div>
+    )
+};
+
+PaginationControls.defaultProps = {
+    onPrevious: () => {},
+    onNext: () => {},
+    max: 1000,
+    disabled: false,
+};
+
+export default PaginationControls;
