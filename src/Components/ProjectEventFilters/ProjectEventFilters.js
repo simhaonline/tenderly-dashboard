79f849425bc117c2954d9844112709bec431efba
Miljan Tekic <miljan.tekic@gmail.com>
2018-12-08 12:11:14 +0100

    Handle search query input update and propagate EventFilter event

--- src/Components/ProjectEventFilters/ProjectEventFilters.js
+++ src/Components/ProjectEventFilters/ProjectEventFilters.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import {initializeForm, updateFormField} from "../../Utils/FormHelpers";
 
 import {Input} from "../../Elements";
+import {EventFilterTypes} from "../../Common/constants";
 
 class ProjectEventFilters extends Component {
     constructor(props) {
@@ -14,19 +15,40 @@ class ProjectEventFilters extends Component {
         this.handleFormUpdate = updateFormField.bind(this);
     }
 
+    /**
+     * @param {string} field
+     * @param {string} value
+     */
+    handleSearchQueryChange = (field, value) => {
+        const {onFiltersChange} = this.props;
+
+        this.handleFormUpdate(field, value);
+
+        onFiltersChange({
+            type: EventFilterTypes.QUERY,
+            value,
+        });
+    };
+
     render() {
         const {formData: {searchQuery}} = this.state;
 
         return (
             <div className="ProjectEventFilters">
-                <div>Contract dropdown</div>
-                <Input icon="search"
-                       label="Search events by txHash, contract address or string"
-                       field="searchQuery"
-                       value={searchQuery} onChange={this.handleFormUpdate}/>
+                <div className="QueryColumn">
+                    <Input icon="search"
+                           label="Search events by txHash, contract address or string"
+                           field="searchQuery"
+                           value={searchQuery} onChange={this.handleSearchQueryChange}/>
+                </div>
+                <div className="DropdownColumn">Contract dropdown</div>
             </div>
         )
     }
 }
+
+ProjectEventFilters.defaultProps = {
+    onFiltersChange: () => {},
+};
 
 export default ProjectEventFilters;
