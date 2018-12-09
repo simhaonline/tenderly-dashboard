90724529a31fa45f4cfb2b85274c1f8747847ad1
Miljan Tekic <miljan.tekic@gmail.com>
2018-12-09 16:19:11 +0100

    Created send feedback and support ticket actions

--- src/Core/App/App.actions.js
+++ src/Core/App/App.actions.js
@@ -1,3 +1,6 @@
+import {ActionResponse} from "../../Common";
+import {Api} from "../../Utils/Api";
+
 export const SET_WHOLE_SCREEN_PAGE_ACTION = 'SET_WHOLE_SCREEN_PAGE';
 
 /**
@@ -9,5 +12,43 @@ export const setWholeScreenPage = (value) => {
             type: SET_WHOLE_SCREEN_PAGE_ACTION,
             value,
         });
+    }
+};
+
+/**
+ * @param {User} user
+ * @param {string} message
+ */
+export const sendFeedback = (user, message) => {
+    return async dispatch => {
+        try {
+            await Api.post('/support/feedback', {
+                subject: `Feedback from ${user.username}`,
+                body: message,
+            });
+
+            return new ActionResponse(true);
+        } catch (error) {
+            return new ActionResponse(false, error)
+        }
+    }
+};
+
+/**
+ * @param {User} user
+ * @param {string} message
+ */
+export const sendSupportTicket = (user, message) => {
+    return async dispatch => {
+        try {
+            await Api.post('/support/ticket', {
+                subject: `Support Ticket from ${user.username}`,
+                body: message,
+            });
+
+            return new ActionResponse(true);
+        } catch (error) {
+            return new ActionResponse(false, error)
+        }
     }
 };
