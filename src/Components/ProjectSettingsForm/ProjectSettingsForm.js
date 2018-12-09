f908f8a4c7a656d8731b87d87c8863078b1ff2c2
Miljan Tekic <miljan.tekic@gmail.com>
2018-12-09 17:37:10 +0100

    Implemented fake update and used the progressive button component

--- src/Components/ProjectSettingsForm/ProjectSettingsForm.js
+++ src/Components/ProjectSettingsForm/ProjectSettingsForm.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-import {Card, CardHeading} from "../../Elements";
+import {initializeForm, updateFormField} from "../../Utils/FormHelpers";
+
+import {Card, CardHeading, Input} from "../../Elements";
+import ProgressiveButton from "../ProgressiveButton/ProgressiveButton";
 
 import './ProjectSettingsForm.css';
 
-const ProjectSettingsForm = ({onSubmit, onChange, project}) => {
-    return (
-        <Card className="ProjectSettingsForm">
-            <CardHeading>
-                <h3>Overview</h3>
-            </CardHeading>
-            <div>{project.name}</div>
-            <div>{project.slug}</div>
-        </Card>
-    );
-};
+class ProjectSettingsForm extends Component {
+    constructor(props) {
+        super(props);
+
+        initializeForm(this, {
+            projectName: props.project.name,
+        });
+        this.handleFormUpdate = updateFormField.bind(this);
+    }
+
+    handleSettingsUpdate = async () => {
+        await new Promise(resolve => setTimeout(resolve, 2000));
+    };
+
+    render() {
+        const {project} = this.props;
+        const {formData: {projectName}} = this.state;
+
+        return (
+            <Card className="ProjectSettingsForm">
+                <CardHeading>
+                    <h3>Overview</h3>
+                </CardHeading>
+                <div>
+                    <Input field="projectName" value={projectName} onChange={this.handleFormUpdate} icon="edit-3" label="Project name"/>
+                </div>
+                <div>{project.slug}</div>
+                <ProgressiveButton label="Save" progressLabel="Saving" color="primary" onClick={this.handleSettingsUpdate}/>
+            </Card>
+        );
+    }
+}
 
 export default ProjectSettingsForm;
