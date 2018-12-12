6c317ba44134fe9dc7bac90d97265e802901887d
Miljan Tekic <miljan.tekic@gmail.com>
2018-12-12 17:02:12 +0100

    Added onclick propagation to PageLink component

--- src/Components/PageLink/PageLink.js
+++ src/Components/PageLink/PageLink.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import {withRouter} from "react-router-dom";
 
-const PageLink = ({history, to, children, staticContext, ...props}) => {
+const PageLink = ({history, onClick, to, children, staticContext, ...props}) => {
+    const handleClick = (event) => {
+        onClick(event);
+        history.push(to);
+    };
+
+
     return (
-        <div onClick={() => { history.push(to) }} {...props}>
+        <div onClick={handleClick} {...props}>
             {children}
         </div>
     );
+};
+
+PageLink.defaultProps = {
+    onClick: () => {},
 };
 
 export default withRouter(PageLink);
