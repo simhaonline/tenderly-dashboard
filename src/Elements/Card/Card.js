c158377f995b0710d1187e0e933dc12ee8130a97
Miljan Tekic <miljan.tekic@gmail.com>
2018-12-14 14:22:07 +0100

    Added clickable state logic to Card component

--- src/Elements/Card/Card.js
+++ src/Elements/Card/Card.js
@@ -3,15 +3,22 @@ import classNames from 'classnames';
 
 import './Card.css';
 
-const Card = ({children, className}) => {
+const Card = ({children, className, clickable}) => {
     return (
         <div className={classNames(
             "Card",
             className,
+            {
+                "Clickable": clickable,
+            },
         )}>
             {children}
         </div>
     )
+};
+
+Card.defaultProps = {
+    clickable: false,
 };
 
 export default Card;
