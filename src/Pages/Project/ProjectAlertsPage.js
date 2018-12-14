45b4c9f60b6ccd417a3e7ea1997adc5f21d27abb
Miljan Tekic <miljan.tekic@gmail.com>
2018-12-14 14:05:55 +0100

    Created basic layout for project alerts page

--- src/Pages/Project/ProjectAlertsPage.js
+++ src/Pages/Project/ProjectAlertsPage.js
@@ -1,15 +1,88 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
 
-import {Container, Page} from "../../Elements";
 import {getProject} from "../../Common/Selectors/ProjectSelectors";
 
+import {Container, Page, Card, CardHeading} from "../../Elements";
+import {PageSegments, PageSegmentSwitcher, PageSegmentContent} from "../../Components";
+
+const PageSegmentsOptions = [
+    {
+        label: 'Events',
+        value: 'events',
+    },
+    {
+        label: 'Analytics',
+        value: 'analytics',
+
+    },
+    {
+        label: 'Deployment',
+        value: 'deployment',
+
+    },
+    {
+        label: 'Integrations',
+        value: 'integrations',
+
+    },
+];
+
 class ProjectAlertsPage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentSegment: 'events',
+        }
+    }
+
+    /**
+     * @param {String} segment
+     */
+    handleSegmentSwitch = (segment) => {
+        this.setState({
+            currentSegment: segment,
+        });
+    };
+
     render() {
+        const {currentSegment} = this.state;
+
         return (
             <Page id="ProjectPage">
                 <Container>
-                    ProjectAlertsPage
+                    <PageSegments>
+                        <PageSegmentSwitcher current={currentSegment} options={PageSegmentsOptions} onSelect={this.handleSegmentSwitch}/>
+                        {currentSegment === 'events' && <PageSegmentContent>
+                            <Card>
+                                <CardHeading>
+                                    <h3>Events</h3>
+                                </CardHeading>
+                            </Card>
+                        </PageSegmentContent>}
+                        {currentSegment === 'analytics' && <PageSegmentContent>
+                            <Card>
+                                <CardHeading>
+                                    <h3>Analytics</h3>
+                                </CardHeading>
+                            </Card>
+                        </PageSegmentContent>}
+                        {currentSegment === 'deployment' && <PageSegmentContent>
+                            <Card>
+                                <CardHeading>
+                                    <h3>Deployment</h3>
+                                </CardHeading>
+                            </Card>
+                        </PageSegmentContent>}
+                        {currentSegment === 'integrations' && <PageSegmentContent>
+                            <Card>
+                                <CardHeading>
+                                    <h3>Integrations</h3>
+                                </CardHeading>
+                            </Card>
+                        </PageSegmentContent>}
+                    </PageSegments>
                 </Container>
             </Page>
         )
