eaf0fdd4ee723852a29c12b668fa81ecc43f3577
Miljan Tekic <miljan.tekic@gmail.com>
2018-12-14 14:08:26 +0100

    Changed feature flag variables

--- src/Components/ProjectNavigation/ProjectNavigation.js
+++ src/Components/ProjectNavigation/ProjectNavigation.js
@@ -32,13 +32,17 @@ const ProjectNavigation = ({project}) => {
                     <NavLink className="NavigationItem" exact to={`/project/${project.id}/contracts`} onClick={() => trackNavigationItem('Contracts')}>
                         <span>Contracts</span>
                     </NavLink>
-                    <FeatureFlag flag={FeatureFlagTypes.COMING_SOON}>
+                    <FeatureFlag flag={FeatureFlagTypes.ANALYTICS}>
                         <NavLink className="NavigationItem" exact to={`/project/${project.id}/analytics`} onClick={() => trackNavigationItem('Analytics')}>
                             <span>Analytics</span>
                         </NavLink>
+                    </FeatureFlag>
+                    <FeatureFlag flag={FeatureFlagTypes.COMING_SOON}>
                         <NavLink className="NavigationItem" exact to={`/project/${project.id}/releases`} onClick={() => trackNavigationItem('Deployment')}>
                             <span>Deployment</span>
                         </NavLink>
+                    </FeatureFlag>
+                    <FeatureFlag flag={FeatureFlagTypes.ALERTS}>
                         <NavLink className="NavigationItem" exact to={`/project/${project.id}/alerts`} onClick={() => trackNavigationItem('Alerts')}>
                             <span>Alerts</span>
                         </NavLink>
