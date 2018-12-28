8cf10d515a987fc9e28e63de5920903c7fba5561
Miljan Tekic <miljan.tekic@gmail.com>
2018-12-28 16:00:49 +0100

    Created password strength component

---
+++ src/Components/PasswordStrengthScore/PasswordStrengthScore.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './PasswordStrengthScore.css';
+
+const PasswordStrengthScore = ({score}) => {
+    return (
+        <div className="PasswordStrengthScore">
+            asd
+        </div>
+    )
+};
+
+export default PasswordStrengthScore;
