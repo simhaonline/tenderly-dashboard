043cb1fdea61c1e4b89fcd7ab59d76f5a4dc94a4
Miljan Tekic <miljan.tekic@gmail.com>
2018-12-28 17:35:17 +0100

    Form style

--- src/Components/RegisterPasswordForm/RegisterPasswordForm.js
+++ src/Components/RegisterPasswordForm/RegisterPasswordForm.js
@@ -4,20 +4,29 @@ import zxcvbn from "zxcvbn";
 import {Button, Card, Input} from "../../Elements";
 import {PasswordStrengthScore} from "../index";
 
+import './RegisterPasswordForm.css';
+
 const RegisterPasswordForm = ({form, onChange, onSubmit}) => {
     const {email, firstName, lastName, password, repeatPassword} = form;
     const passwordStrength = zxcvbn(password, [email, firstName, lastName]);
 
     return (
         <Card className="RegisterPasswordForm">
-            <Input icon="lock" type="password" label="Password" field="password" value={password} onChange={onChange}/>
-            {password && <div>
-                <p>Hackable time: {passwordStrength.crack_times_display.offline_slow_hashing_1e4_per_second}</p>
-                <p>Score: {passwordStrength.score}</p>
-            </div>}
-            <Input icon="lock" type="password" label="Repeat password" field="repeatPassword" value={repeatPassword} onChange={onChange}/>
-            <PasswordStrengthScore score={passwordStrength.score}/>
-            <Button>
+            <div className="MainFormWrapper">
+                <div className="InputsWrapper">
+                    <div>
+                        <Input icon="lock" type="password" label="Password" field="password" value={password} onChange={onChange}/>
+                        <Input icon="lock" type="password" label="Repeat password" field="repeatPassword" value={repeatPassword} onChange={onChange}/>
+                    </div>
+                    {password && <div className="CrackInfoWrapper">
+                        <div className="CrackInfo">Hackable time: <span className="CrackTimeInfo">{passwordStrength.crack_times_display.offline_slow_hashing_1e4_per_second}</span></div>
+                    </div>}
+                </div>
+                <div className="ScoreWrapper">
+                    <PasswordStrengthScore score={passwordStrength.score}/>
+                </div>
+            </div>
+            <Button disabled={!password || !repeatPassword}>
                 <span>Create Account</span>
             </Button>
         </Card>
