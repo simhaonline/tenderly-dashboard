5a4cad7ca1eaf9135264aad2a6c2825578a3bb9c
Miljan Tekic <miljan.tekic@gmail.com>
2018-12-30 13:54:01 +0100

    connected inputs

--- src/Components/RegisterAccountForm/RegisterAccountForm.js
+++ src/Components/RegisterAccountForm/RegisterAccountForm.js
@@ -7,10 +7,10 @@ import './RegisterAccountForm.css';
 const RegisterAccountForm = ({form, onChange, onSubmit}) => {
     return (
         <Card className="RegisterAccountForm">
-            <Form>
-                <Input/>
-                <Input/>
-                <Input/>
+            <Form onSubmit={onSubmit}>
+                <Input field="firstName" onChange={onChange} value={form.firstName} label="First name" icon="user"/>
+                <Input field="lastName" onChange={onChange} value={form.lastName} label="Last name" icon="user"/>
+                <Input field="email" onChange={onChange} value={form.email} label="E-mail" icon="mail"/>
                 <Button onClick={onSubmit}>
                     <span>Next</span>
                     <Icon icon="arrow-right"/>
