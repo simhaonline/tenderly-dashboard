7cc9ff5137a100f008942ad965648bf83a4f11fd
Miljan Tekic <miljan.tekic@gmail.com>
2019-01-18 16:00:53 +0000

    GitHub login button now redirects to the oauth app

--- src/Components/GitHubLoginButton/GitHubLoginButton.js
+++ src/Components/GitHubLoginButton/GitHubLoginButton.js
@@ -1,15 +1,29 @@
 import React from 'react';
 
+import {APP_BASE_URL, GITHUB_CLIENT_ID, OAuthServiceTypeMap} from "../../Common/constants";
+
 import {Icon} from "../../Elements";
 
 import './GitHubLoginButton.css';
 
-const GitHubButton = ({...props}) => {
+const GITHUB_BASE_URL = 'https://github.com/login/oauth/authorize';
+const GITHUB_REDIRECT_URL = `${APP_BASE_URL}/oauth/${OAuthServiceTypeMap.GITHUB}`;
+
+const GitHubButton = ({onAuthentication, ...props}) => {
+    const GitHubData = {
+        client_id: GITHUB_CLIENT_ID,
+        redirect_uri: GITHUB_REDIRECT_URL,
+    };
+
+    const searchParams = new URLSearchParams(GitHubData);
+
+    const loginUri = `${GITHUB_BASE_URL}?${searchParams.toString()}`;
+
     return (
-        <button className="GitHubLoginButton" {...props}>
+        <a className="GitHubLoginButton" {...props} href={loginUri}>
             <Icon icon="github" className="GitHubIcon"/>
             <span>Login with GitHub</span>
-        </button>
+        </a>
     )
 };
 
