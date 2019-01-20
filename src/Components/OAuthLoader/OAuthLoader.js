fc3bef7d15ba5892f8d555b96364feaa8f122f06
Miljan Tekic <miljan.tekic@gmail.com>
2019-01-20 16:20:38 +0000

    Extracted service labels to constants

--- src/Components/OAuthLoader/OAuthLoader.js
+++ src/Components/OAuthLoader/OAuthLoader.js
@@ -2,20 +2,15 @@ import React from 'react';
 
 import {CircularLoader} from "../Loaders/Loaders";
 
-import {OAuthServiceTypeMap} from "../../Common/constants";
+import {OAuthServiceLabelMap} from "../../Common/constants";
 
 import './OAuthLaoder.css';
-
-const ServiceToLabelMap = {
-    [OAuthServiceTypeMap.GOOGLE]: 'Google',
-    [OAuthServiceTypeMap.GITHUB]: 'GitHub',
-};
 
 const OAuthLoader = ({service}) => {
   return (
       <div className="OAuthLoader">
           <CircularLoader/>
-          <div className="LoadingMessage">Authenticating via {ServiceToLabelMap[service]}</div>
+          <div className="LoadingMessage">Authenticating via {OAuthServiceLabelMap[service]}</div>
       </div>
   )
 };
