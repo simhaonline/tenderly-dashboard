4e323a323959fc966389f7225bcf04387e591ed8
Miljan Tekic <miljan.tekic@gmail.com>
2019-01-20 15:26:23 +0000

    Created loader components

---
+++ src/Components/Loaders/Loaders.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Loaders.css';
+
+export const CircularLoader = () => {
+    return (
+        <div className="CircularLoader">
+            <div className="LoadingSpinner"/>
+        </div>
+    )
+};
