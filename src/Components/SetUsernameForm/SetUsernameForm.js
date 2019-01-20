79f5bf062fadc16fc149872507135c5bd418869f
Miljan Tekic <miljan.tekic@gmail.com>
2019-01-20 16:32:25 +0000

    Empty set username form component

---
+++ src/Components/SetUsernameForm/SetUsernameForm.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import {Card} from "../../Elements";
+
+class SetUsernameForm extends Component {
+    render() {
+        return (
+            <Card>
+                set me
+            </Card>
+        )
+    }
+}
+
+export default SetUsernameForm;
