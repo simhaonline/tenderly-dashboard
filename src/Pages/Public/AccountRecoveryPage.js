eb203435285d03ed262cba0a4739d286e9dfab6f
Miljan Tekic <miljan.tekic@gmail.com>
2019-02-13 11:48:28 +0100

    Finished the account recovery page

--- src/Pages/Public/AccountRecoveryPage.js
+++ src/Pages/Public/AccountRecoveryPage.js
@@ -1,17 +1,37 @@
 import React, {Component} from 'react';
+import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
+
+import * as authActions from "../../Core/Auth/Auth.actions";
 
 import {Container, Page} from "../../Elements";
+import {AccountRecoveryForm} from "../../Components";
 
 class AccountRecoveryPage extends Component {
+    handleRecoverAccountSubmit = async ({email}) => {
+        const {actions} = this.props;
+
+        return actions.recoverAccount(email);
+    };
+
     render() {
         return (
-            <Page id="AccountRecoveryPage">
+            <Page id="AccountRecoveryPage" wholeScreenPage>
                 <Container>
-                    <h1>teting</h1>
+                    <AccountRecoveryForm onSubmit={this.handleRecoverAccountSubmit}/>
                 </Container>
             </Page>
         )
     }
 }
 
-export default AccountRecoveryPage;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        actions: bindActionCreators(authActions, dispatch),
+    }
+};
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(AccountRecoveryPage);
