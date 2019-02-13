9b1025db7f13cd40d89bbeab1116070e1f87c81d
Miljan Tekic <miljan.tekic@gmail.com>
2019-02-13 11:19:25 +0100

    Created form validators file and created email validation

---
+++ src/Utils/FormValidators.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} email
+ * @return {boolean}
+ */
+export function ValidateEmail(email) {
+    // eslint-disable-next-line
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(String(email).toLowerCase());
+}
