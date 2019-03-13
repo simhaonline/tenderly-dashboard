17c2705f44b5f3617035d0fe201a8900b22f9597
Miljan Tekic <miljan.tekic@gmail.com>
2019-03-13 17:25:26 +0100

    Set cookie on main domain

--- src/Core/Auth/Auth.actions.js
+++ src/Core/Auth/Auth.actions.js
@@ -23,9 +23,9 @@ export const SET_USERNAME_ACTION = 'SET_USERNAME';
 const setAuthHeader = (token) => {
     return async dispatch => {
         if (process.env.NODE_ENV === 'production') {
-            Cookies.set('token', token, { path: '/', expires: 30 });
+            Cookies.set('token', token, { path: '/', expires: 30, domain: '.tenderly.app'});
         } else {
-            Cookies.set('token', token, { path: '/', expires: 30, domain: '.tenderly.app' });
+            Cookies.set('token', token, { path: '/', expires: 30});
         }
 
         Api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
