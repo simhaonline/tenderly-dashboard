c96d9f313edceb9d211912881e879d607d86def7
Miljan Tekic <miljan.tekic@gmail.com>
2019-03-21 22:00:52 +0100

    Finished styling for coming soon

--- src/Components/FeatureComingSoon/FeatureComingSoon.js
+++ src/Components/FeatureComingSoon/FeatureComingSoon.js
@@ -1,12 +1,24 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+
+import {Icon} from "../../Elements";
 
 import './FeatureComingSoon.scss';
 
 class FeatureComingSoon extends Component {
     render() {
+        const {feature} = this.props;
+
         return (
             <div className="FeatureComingSoon">
-                lalal
+                {feature === 'alerting' && <Fragment>
+                    <Icon icon="alerting" className="FeatureIcon"/>
+                    <div className="FeatureName">Alerting</div>
+                </Fragment>}
+                {feature === 'analytics' && <Fragment>
+                    <Icon icon="analytics" className="FeatureIcon"/>
+                    <div className="FeatureName">Analytics</div>
+                </Fragment>}
+                <div className="FeatureDescription">This feature is currently in development. Stay tuned for more information.</div>
             </div>
         );
     }
