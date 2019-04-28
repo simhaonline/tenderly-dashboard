c3e4a9420f18aa7dac200c094a89795d69ea6806
Miljan Tekic <miljan.tekic@gmail.com>
2019-04-28 21:25:16 +0200

    Export network tag

--- src/Components/index.js
+++ src/Components/index.js
@@ -52,6 +52,7 @@ export {CircularLoader, DotLoader, EthereumLoader, SimpleLoader} from './Loaders
 export {default as ProjectPageLoader} from './ProjectPageLoader/ProjectPageLoader';
 export {default as ProjectContentLoader} from './ProjectContentLoader/ProjectContentLoader';
 export {default as NetworkSegmentedPicker} from './NetworkSegmentedPicker/NetworkSegmentedPicker';
+export {default as NetworkTag} from './NetworkTag/NetworkTag';
 export {default as FeatureComingSoon} from './FeatureComingSoon/FeatureComingSoon';
 export {default as UserInformationForm} from './UserInformationForm/UserInformationForm';
 export {default as TransactionsList} from './TransactionsList/TransactionsList';
