3e48df6af5a11e38ffc695eb0a213c1536c3ff49
Miljan Tekic <miljan.tekic@gmail.com>
2019-04-28 22:06:56 +0200

    Prop types for icon

--- src/Elements/Icon/Icon.js
+++ src/Elements/Icon/Icon.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
 import './Icon.css';
 
@@ -9,6 +10,11 @@ const Icon = ({icon, className, ...props}) => {
     return (
         <i className={classNames('TnIcon', iconClass, className)} {...props}/>
     )
+};
+
+Icon.propTypes = {
+    icon: PropTypes.string.isRequired,
+    className: PropTypes.string,
 };
 
 export default Icon;
