07dab9c84cbe64cc13ba4ac63627b7cd90336fee
Miljan Tekic <miljan.tekic@gmail.com>
2019-05-05 17:48:55 +0200

    Save all transaction in memory

--- src/Core/Transaction/Transaction.reducer.js
+++ src/Core/Transaction/Transaction.reducer.js
@@ -1,12 +1,28 @@
 import {LOG_OUT_ACTION} from "../Auth/Auth.actions";
+import {FETCH_TRANSACTION_ACTION, FETCH_TRANSACTIONS_FOR_PROJECT_ACTION} from "./Transaction.actions";
 
 const initialState = {
     transactions: {},
-    projectTransactions: {},
 };
 
 const TransactionReducer = (state = initialState, action) => {
     switch (action.type) {
+        case FETCH_TRANSACTIONS_FOR_PROJECT_ACTION:
+            const transactions = {};
+
+            action.transactions.forEach(tx => {
+                transactions[tx.txHash] = tx;
+            });
+
+            return {
+                ...state,
+                transactions: {
+                    ...state.transactions,
+                    ...transactions,
+                }
+            };
+        case FETCH_TRANSACTION_ACTION:
+            return state;
         case LOG_OUT_ACTION:
             return initialState;
         default:
