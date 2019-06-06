a34587b181a0e457c2429ce644c936548f981eca
Miljan Tekic <miljan.tekic@gmail.com>
2019-06-06 17:08:27 +0200

    use line to make key for component mapping

--- src/Components/EventStackTrace/EventStackTrace.js
+++ src/Components/EventStackTrace/EventStackTrace.js
@@ -13,7 +13,7 @@ const EventStackTrace = ({trace, source}) => {
     return (
         <div className="EventStackTrace">
             <div className="EventStackTraceHeading"><h5>StackTrace</h5></div>
-            {trace && trace.map((point, index) => <EventStackTracePoint open={index === 0} key={index + point.start} point={point} source={source}/>)}
+            {trace && trace.map((point, index) => <EventStackTracePoint open={index === 0} key={index + point.line} point={point} source={source}/>)}
         </div>
     )
 };
