a101190924080cb46a580092e0cf73bc211f48ff
Miljan Tekic <miljan.tekic@gmail.com>
2019-06-07 16:33:52 +0200

    Finished action for fetching single contract

--- src/Core/Contract/Contract.actions.js
+++ src/Core/Contract/Contract.actions.js
@@ -1,7 +1,10 @@
 import {Api} from "../../Utils/Api";
 import Contract from './Contract.model';
+import {ErrorActionResponse, SuccessActionResponse} from "../../Common";
+import {NetworkAppToApiTypeMap} from "../../Common/constants";
 
 export const FETCH_CONTRACTS_FOR_PROJECT_ACTION = 'FETCH_CONTRACTS_FOR_PROJECT';
+export const FETCH_CONTRACT_FOR_PROJECT_ACTION = 'FETCH_CONTRACT_FOR_PROJECT';
 
 /**
  * @param {string} projectId
@@ -17,6 +20,7 @@ export const fetchContractsForProject = (projectId, account = null) => {
             const {data} = await Api.get(`/account/${projectAccount}/project/${projectId}/contracts`);
 
             if (!data) {
+                // @TODO Return an ErrorActionResponse here.
                 return null;
             }
 
@@ -28,11 +32,47 @@ export const fetchContractsForProject = (projectId, account = null) => {
                 projectId,
             });
 
+            // @TODO Return an SuccessActionResponse here.
             return contracts;
         } catch (error) {
             console.error(error);
+            // @TODO Return an ErrorActionResponse here.
             return null;
         }
     }
 };
 
+/**
+ *
+ * @param {string} projectId
+ * @param {string} contactAddress
+ * @param {NetworkTypes} network
+ */
+export const fetchContractForProject = (projectId, contactAddress, network) => {
+    return async (dispatch, getState) => {
+        const {auth: {user: {username}}} = getState();
+
+        try {
+            const apiNetworkId = NetworkAppToApiTypeMap[network];
+
+            const {data} = await Api.get(`/account/${username}/project/${projectId}/contract/${apiNetworkId}/${contactAddress}`);
+
+            if (!data) {
+                return new ErrorActionResponse();
+            }
+
+            const contract = Contract.buildFromResponse(data, projectId);
+
+            await dispatch({
+                type: FETCH_CONTRACT_FOR_PROJECT_ACTION,
+                contract,
+                projectId,
+            });
+
+            return new SuccessActionResponse(contract);
+        } catch (error) {
+            console.error(error);
+            return new ErrorActionResponse(error);
+        }
+    }
+};
