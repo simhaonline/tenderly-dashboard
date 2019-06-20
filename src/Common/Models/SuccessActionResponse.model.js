4f91fa7cb812c22abf8e883a938fc1cf4c361072
Miljan Tekic <miljan.tekic@gmail.com>
2019-06-20 12:00:13 +0200

    optional data param

--- src/Common/Models/SuccessActionResponse.model.js
+++ src/Common/Models/SuccessActionResponse.model.js
@@ -2,7 +2,7 @@ import ActionResponse from "./ActionResponse.model";
 
 export default class SuccessActionResponse extends ActionResponse {
     /**
-     * @param {*} data
+     * @param {*} [data]
      */
     constructor(data) {
         super(true, data);
