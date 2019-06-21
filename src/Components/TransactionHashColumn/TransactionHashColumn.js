732bf2d084691dc0ab803302ff97bb5b7a40fa0e
Miljan Tekic <miljan.tekic@gmail.com>
2019-06-21 13:54:45 +0200

    implemented tooltips for internal transaction type

--- src/Components/TransactionHashColumn/TransactionHashColumn.js
+++ src/Components/TransactionHashColumn/TransactionHashColumn.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
 
 import {generateShortAddress} from "../../Utils/AddressFormatter";
 
-import {Icon} from "../../Elements";
+import {Icon, Tooltip} from "../../Elements";
 
 import './TransactionHashColumn.scss';
 
 const TransactionHashColumn = ({transaction, displayType, contracts}) => {
+    const id = `InternalTxTooltip__${transaction.txHash}`;
+
     return (
         <div className="TransactionHashColumn DisplayFlex AlignItemsCenter">
             <span className="TransactionHashColumn__Address">{generateShortAddress(transaction.txHash, 12, 6)}</span>
-            {displayType && transaction.isInternalTransaction(contracts) && <div className="TransactionHashColumn__TypeWrapper MarginLeft2">
-                <Icon icon="minimize-2"/>
-            </div>}
+            {displayType && transaction.isInternalTransaction(contracts) && <Fragment>
+                <div className="TransactionHashColumn__TypeWrapper MarginLeft2" id={id} onClick={event => event.stopPropagation()}>
+                    <Icon icon="minimize-2"/>
+                </div>
+                <Tooltip id={id}>
+                    <span>Internal Transaction</span>
+                </Tooltip>
+            </Fragment>}
         </div>
     )
 };
