23f274f57e9ac74f64a9f60255c888eeae4956cf
Miljan Tekic <miljan.tekic@gmail.com>
2019-07-19 15:55:26 +0200

    logged in styling for mobile

--- src/Pages/PublicContracts/PublicContractPage.js
+++ src/Pages/PublicContracts/PublicContractPage.js
@@ -88,6 +88,10 @@ class PublicContractPage extends Component {
     };
 
     handlePageChange = (nextPage) => {
+        const {loggedIn} = this.props;
+
+        console.log(loggedIn);
+
         this.setState({
             page: nextPage,
         }, () => {
