99587ed1a0cda39999149552a0dd2dcf3a2bf06d
Miljan Tekic <miljan.tekic@gmail.com>
2019-07-26 16:31:16 +0200

    Stylign for public contract quick actions

--- src/Components/PublicContractQuickActions/PublicContractQuickActions.js
+++ src/Components/PublicContractQuickActions/PublicContractQuickActions.js
@@ -1,31 +1,100 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import {connect} from "react-redux";
 
 import {FeatureFlagTypes} from "../../Common/constants";
 
 import {Card, Icon} from "../../Elements";
-import {FeatureFlag} from "../index";
+import {FeatureFlag, LoginRequiredModal} from "../index";
+
+import './PublicContractQuickActions.scss';
 
 class PublicContractQuickActions extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loginModalOpen: false,
+            actionType: null,
+            actionModalOpen: false,
+        }
+    }
+
+    openLoginModal = () => {
+        this.setState({
+            loginModalOpen: true,
+        });
+    };
+
+    closeLoginModal = () => {
+        this.setState({
+            loginModalOpen: false,
+        });
+    };
+
+    openActionModal = (actionType) => {
+        this.setState({
+            actionModalOpen: true,
+            actionType,
+        });
+    };
+
+    closeActionModal = () => {
+        this.setState({
+            actionModalOpen: false,
+        });
+    };
+
+    handleActionClick = (actionType) => {
+        const {loggedIn} = this.props;
+
+        if (!loggedIn) {
+            this.openLoginModal();
+            return;
+        }
+
+        this.openActionModal(actionType);
+    };
+
     render() {
+        const {loginModalOpen} = this.state;
+
         return (
-            <div className="DisplayFlex MarginBottom4">
-                <Card onClick={() => {}} className="PublicContractQuickAction">
-                    <Icon icon="project"/>
-                    <span>Add to Project</span>
-                </Card>
-                <FeatureFlag flag={FeatureFlagTypes.ALERTS}>
-                    <Card onClick={() => {}} className="PublicContractQuickAction">
-                        <Icon icon="alerting"/>
-                        <span>Setup Alerts</span>
+            <Fragment>
+                <div className="PublicContractQuickActions">
+                    <Card onClick={() => this.handleActionClick('add-to-project')} className="PublicContractQuickActions__Action">
+                        <div className="PublicContractQuickActions__Action__Icon">
+                            <Icon icon="project"/>
+                        </div>
+                        <span>Add to Project</span>
                     </Card>
-                </FeatureFlag>
-                <Card onClick={() => {}} className="PublicContractQuickAction">
-                    <Icon icon="filter"/>
-                    <span>Filter Transactions</span>
-                </Card>
-            </div>
+                    <FeatureFlag flag={FeatureFlagTypes.ALERTS}>
+                        <Card onClick={() => this.handleActionClick('setup-alerts')} className="PublicContractQuickActions__Action">
+                            <div className="PublicContractQuickActions__Action__Icon">
+                                <Icon icon="alerting"/>
+                            </div>
+                            <span>Setup Alerts</span>
+                        </Card>
+                    </FeatureFlag>
+                    <Card onClick={() => this.handleActionClick ('filter-transactions')} className="PublicContractQuickActions__Action">
+                        <div className="PublicContractQuickActions__Action__Icon">
+                            <Icon icon="filter"/>
+                        </div>
+                        <span>Filter Transactions</span>
+                    </Card>
+                </div>
+                <LoginRequiredModal open={loginModalOpen} onClose={this.closeLoginModal}/>
+            </Fragment>
         );
     }
 }
 
-export default PublicContractQuickActions;
+const mapStateToProps = (state) => {
+    return {
+        loggedIn: state.auth.loggedIn,
+    }
+};
+
+export default connect(
+    mapStateToProps,
+    null
+)(PublicContractQuickActions);
