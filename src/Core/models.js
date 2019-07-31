f96332a5df51eda0ca2119c094fa8dfc37769056
Miljan Tekic <miljan.tekic@gmail.com>
2019-07-31 11:54:56 +0200

    Exporting models

--- src/Core/models.js
+++ src/Core/models.js
@@ -2,3 +2,6 @@ export {default as User} from './Auth/User.model';
 export {default as Contract} from './Contract/Contract.model';
 export {default as CallTrace} from './Trace/CallTrace.model';
 export {default as Transaction} from './Transaction/Transaction.model';
+export {default as Alert} from './Alerting/Alert.model';
+export {default as AlertRule} from './Alerting/AlertRule.model';
+export {default as AlertRuleExpression} from './Alerting/AlertRuleExpression.model';
