0d366067f11d1435c2eb5e6dfbe82d31b41afe4b
Miljan Tekic <miljan.tekic@gmail.com>
2019-08-04 13:16:06 +0200

    Selector for alert rule destinations

--- src/Common/Selectors/NotificationSelectors.js
+++ src/Common/Selectors/NotificationSelectors.js
@@ -8,6 +8,21 @@ export function getNotificationDestinations(state) {
 
 /**
  * @param {Object} state
+ * @param {AlertRule} rule
+ * @return {NotificationDestination[]}
+ */
+export function getNotificationDestinationsForRule(state, rule) {
+    if (!rule) {
+        return [];
+    }
+
+    const destinations = getNotificationDestinations(state);
+
+    return destinations.filter(destination => rule.deliveryChannels.includes(destination.id));
+}
+
+/**
+ * @param {Object} state
  * @return {boolean}
  */
 export function areNotificationDestinationsLoaded(state) {
