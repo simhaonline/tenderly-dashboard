0b89e013c9149ca2c302c2314d9c906f7224ae38
Miljan Tekic <miljan.tekic@gmail.com>
2019-08-07 13:08:58 +0200

    Split tx contract into component

--- src/Components/TransactionContracts/TransactionContracts.js
+++ src/Components/TransactionContracts/TransactionContracts.js
@@ -5,10 +5,49 @@ import {Contract} from "../../Core/models";
 
 import {getContractCompany, isRecognizedCompanyContract} from "../../Utils/CompanyContracts";
 
-import {PanelDivider, Card} from "../../Elements";
+import {PanelDivider, Icon, Card, Tag, LinkButton} from "../../Elements";
 import {CompanyLogo} from "..";
 
+import './TransactionContracts.scss';
+import {generateShortAddress} from "../../Utils/AddressFormatter";
+
+const TransactionContract = ({contract}) => {
+    return (
+        <Card color="light" className="TransactionContracts__Card" clickable>
+            <div className="SemiBoldText">{contract.name}</div>
+            <div className="MonospaceFont LinkText">{generateShortAddress(contract.address, 8)}</div>
+            <div>
+                {!contract.isPublic && <Tag size="small">
+                    <Icon icon="project" className="TransactionContracts__Card__TagIcon"/>
+                    <span>Project Contract</span>
+                </Tag>}
+                {contract.isPublic && contract.isVerifiedPublic && <Tag size="small" color="success">
+                    <Icon icon='check-circle'/>
+                    <span>Verified Contract</span>
+                </Tag>}
+                {contract.isPublic && !contract.isVerifiedPublic && <Tag size="small" color="warning">
+                    <Icon icon='x-circle'/>
+                    <span>Unverified Contract</span>
+                </Tag>}
+                {contract.isPublic && contract.isVerifiedPublic && <LinkButton>
+                    <Icon icon='plus'/>
+                    <span>Add to Project</span>
+                </LinkButton>}
+            </div>
+        </Card>
+    );
+};
+
 class TransactionContracts extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedContract: null,
+            selectedFile: null,
+        };
+    }
+
     render() {
         const {contracts} = this.props;
 
@@ -31,20 +70,14 @@ class TransactionContracts extends Component {
         });
 
         return (
-            <div>
+            <div className="TransactionContracts">
                 {Object.keys(computedData.companyContracts).map(company => <div key={company}>
                     <CompanyLogo company={company}/>
                     <PanelDivider/>
-                    {computedData.companyContracts[company].map(contract => <Card key={contract.address} color="light">
-                        {contract.name}
-                        <div>{contract.isPublic ? 'Public' : 'Project'}</div>
-                    </Card>)}
+                    {computedData.companyContracts[company].map(contract => <TransactionContract contract={contract} key={contract.address}/>)}
                 </div>)}
                 <h4>Other Contracts</h4>
-                {computedData.otherContracts.map(contract => <Card key={contract.address} color="light">
-                    {contract.name}
-                    <div>{contract.isPublic ? 'Public' : 'Project'}</div>
-                </Card>)}
+                {computedData.otherContracts.map(contract => <TransactionContract contract={contract} key={contract.address}/>)}
             </div>
         );
     }
