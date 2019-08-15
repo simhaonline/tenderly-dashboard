99fd63e04cb37f379346a7753314c76c6d11a010
Miljan Tekic <miljan.tekic@gmail.com>
2019-08-15 12:04:59 +0200

    Disabled and highlight color for card component

--- src/Elements/Card/Card.js
+++ src/Elements/Card/Card.js
@@ -8,16 +8,22 @@ const CardColorClassMap = {
     'light': 'Card--Light',
 };
 
-const Card = ({children, className, color, clickable, selectable, selected, onClick}) => {
+const CardHighlightColorClassMap = {
+    'secondary': 'Card--HighlightSecondary',
+};
+
+const Card = ({children, className, color, clickable, selectable, selected, onClick, highlightColor, disabled}) => {
     return (
         <div className={classNames(
             "Card",
             className,
+            CardHighlightColorClassMap[highlightColor],
             CardColorClassMap[color],
             {
                 "Card--Clickable": clickable,
                 "Card--Selectable": selectable,
                 "Card--Selected": selected,
+                "Card--Disabled": disabled,
             },
         )} onClick={onClick}>
             {children}
@@ -29,6 +35,8 @@ Card.propTypes = {
     clickable: PropTypes.bool,
     selectable: PropTypes.bool,
     selected: PropTypes.bool,
+    disabled: PropTypes.bool,
+    highlightColor: PropTypes.oneOf(['secondary']),
     color: PropTypes.oneOf(['light']),
 };
 
