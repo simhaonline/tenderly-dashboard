592e41a8c02554e2d05ed20b91e6bcaa24c804e6
Miljan Tekic <miljan.tekic@gmail.com>
2019-08-28 14:02:25 +0200

    created DialogLoader component for dialogs

---
+++ src/Elements/Dialog/DialogLoader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {SimpleLoader} from "../../Components";
+
+const DialogLoader = () => {
+    return (
+        <div className="DialogLoader">
+            <SimpleLoader/>
+        </div>
+    );
+};
+
+export default DialogLoader;
--- src/Elements/index.js
+++ src/Elements/index.js
@@ -5,6 +5,7 @@ export {default as Header} from './Header/Header';
 export {default as Dialog} from './Dialog/Dialog';
 export {default as DialogBody} from './Dialog/DialogBody';
 export {default as DialogHeader} from './Dialog/DialogHeader';
+export {default as DialogLoader} from './Dialog/DialogLoader';
 export {default as Icon} from './Icon/Icon';
 export {default as Code} from './Code/Code';
 export {default as Alert} from './Alert/Alert';
