161517f20b7dac263d49d0c6e01550381009b36f
Miljan Tekic <miljan.tekic@gmail.com>
2019-08-29 11:53:22 +0200

    reexport stacktrace model

--- src/Core/models.js
+++ src/Core/models.js
@@ -3,6 +3,7 @@ export {default as Organization} from './Organization/Organization.model';
 export {default as Contract} from './Contract/Contract.model';
 export {default as ContractMethod} from './Contract/ContractMethod.model';
 export {default as CallTrace} from './Trace/CallTrace.model';
+export {default as StackTrace} from './Trace/StackTrace.model';
 export {default as Transaction} from './Transaction/Transaction.model';
 export {default as Alert} from './Alerting/Alert.model';
 export {default as AlertRule} from './Alerting/AlertRule.model';
