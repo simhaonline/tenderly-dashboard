958a1d897c2b71deaf15ef7d6df6e250343ad95e
Miljan Tekic <miljan.tekic@gmail.com>
2019-08-29 14:25:10 +0200

    Logic for displaying stack trace

--- src/Components/TransactionStackTrace/TransactionStackTrace.js
+++ src/Components/TransactionStackTrace/TransactionStackTrace.js
@@ -1,15 +1,62 @@
-import React from 'react';
+import React, {Fragment, PureComponent} from 'react';
 import PropTypes from 'prop-types';
+import * as _ from "lodash";
 
 import {StackTrace} from "../../Core/models";
 
-const TransactionStackTrace = ({stackTrace}) => {
+import {Panel, PanelContent, LinkButton} from "../../Elements";
+
+import './TransactionStackTrace.scss';
+
+/**
+ * @param {Trace} trace
+ * @param {Contract[]} contracts
+ * @constructor
+ */
+const TransactionStackTraceLine = ({trace, contracts, first, last}) => {
+    const contract = contracts.find(contract => trace.contract === contract.address);
+    let file;
+
+    if (contract && trace.fileId !== null) {
+        file = contract.getFileById(trace.fileId);
+    }
+
     return (
-        <div className="TransactionStackTrace">
-            Stack Trace
+        <div className="TransactionStackTraceLine">
+            {!!file && <div>
+                {!first && <div>{trace.functionName.trim()}()</div>}
+                {first && <div>{file.source.split('\n')[trace.lineNumber - 1].trim()}</div>}
+                <div>at <LinkButton>{contract.name}:{trace.lineNumber}</LinkButton></div>
+            </div>}
+            {!file && <div>
+                <div>Opcode: [{trace.op}]</div>
+                <div>in <LinkButton>{trace.contract}</LinkButton></div>
+            </div>}
         </div>
-    );
+    )
+
 };
+
+class TransactionStackTrace extends PureComponent {
+    render() {
+        const {stackTrace, contracts} = this.props;
+
+        return (
+            <Fragment>
+                <h2 className="MarginBottom2 MarginLeft2">Stack Trace</h2>
+                <Panel className="TransactionStackTrace">
+                    <PanelContent>
+                        <div className="TransactionStackTrace__Wrapper">
+                            {stackTrace.trace.map((trace, index) =>
+                                <TransactionStackTraceLine trace={trace} contracts={contracts} key={index} first={index === 0} last={index === (stackTrace.trace.length - 1)}/>
+                            )}
+                        </div>
+                    </PanelContent>
+                </Panel>
+            </Fragment>
+        );
+    }
+}
 
 TransactionStackTrace.propTypes = {
     stackTrace: PropTypes.instanceOf(StackTrace).isRequired,
