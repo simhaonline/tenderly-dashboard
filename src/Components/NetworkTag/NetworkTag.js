19c24239c99ab1ddf23b49ea9365348d593b9574
Miljan Tekic <miljan.tekic@gmail.com>
2019-09-02 14:37:53 +0200

    NetworkTag shorthand support

--- src/Components/NetworkTag/NetworkTag.js
+++ src/Components/NetworkTag/NetworkTag.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-import {NetworkLabelMap, NetworkTypes} from "../../Common/constants";
+import {NetworkLabelMap, NetworkLabelShorthandMap, NetworkTypes} from "../../Common/constants";
 
 import './NetworkTag.scss';
 
@@ -17,10 +17,14 @@ const networkTagSizeToClassNameMap = {
     small: 'NetworkTag--Small',
 };
 
-const NetworkTag = ({size, network, prefix, className, short}) => {
+const NetworkTag = ({size, network, prefix, className, useShorthand}) => {
     let networkLabel = NetworkLabelMap[network];
     const networkClassName = networkTypeToClassNameMap[network];
     const sizeClassName = networkTagSizeToClassNameMap[size];
+
+    if (useShorthand) {
+        networkLabel = NetworkLabelShorthandMap[network];
+    }
 
     if (prefix) {
         networkLabel = `${prefix} ${networkLabel}`;
@@ -43,6 +47,7 @@ NetworkTag.propTypes = {
     size: PropTypes.string,
     prefix: PropTypes.string,
     className: PropTypes.string,
+    useShorthand: PropTypes.bool,
 };
 
 export default NetworkTag;
