e2d61b05ae240867869cc2766a81fb74b23b5e3a
Miljan Tekic <miljan.tekic@gmail.com>
2019-09-03 12:36:59 +0200

    Add tooltip for network shorthand

--- src/Components/SelectComponents/ContractSelectMultiValueLabel.js
+++ src/Components/SelectComponents/ContractSelectMultiValueLabel.js
@@ -2,20 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {components} from "react-select";
 
+import {Tooltip} from "../../Elements";
 import {NetworkTag} from "..";
 
 import './ContractSelectMultiValueLabel.scss';
+import {NetworkLabelMap} from "../../Common/constants";
 
 function ContractSelectMultiValueLabel(props) {
     const {data} = props;
 
     return (
         <components.MultiValueLabel {...props}>
-            <div className="ContractSelectMultiValueLabel">
-                <NetworkTag network={data.network} useShorthand size="small" className="ContractSelectMultiValueLabel__Network"/>
+            <div className="ContractSelectMultiValueLabel" >
+                <NetworkTag network={data.network} id={data.value.replace(':', '_')} useShorthand size="small" className="ContractSelectMultiValueLabel__Network"/>
                 <div className="ContractSelectMultiValueLabel__Name">
                     {data.label}
                 </div>
+                <Tooltip id={data.value.replace(':', '_')} placement="top">
+                    {NetworkLabelMap[data.network]}
+                </Tooltip>
             </div>
         </components.MultiValueLabel>
     );
