1c8afba686ca744b2091a98a448589b2d8a1b7c3
Miljan Tekic <miljan.tekic@gmail.com>
2019-09-03 10:18:47 +0200

    Persist transaction filtering in url

--- src/Pages/Project/ProjectTransactionsPage.js
+++ src/Pages/Project/ProjectTransactionsPage.js
@@ -6,7 +6,7 @@ import _ from 'lodash';
 
 import {areProjectContractsLoaded, getProject} from "../../Common/Selectors/ProjectSelectors";
 import {getContractsForProject} from "../../Common/Selectors/ContractSelectors";
-import {ONE_MIN_INTERVAL, ProjectTypes} from "../../Common/constants";
+import {ONE_MIN_INTERVAL, ProjectTypes, TransactionFilterTypes} from "../../Common/constants";
 import Notifications from "../../Utils/Notifications";
 
 import * as transactionActions from "../../Core/Transaction/Transaction.actions";
@@ -15,18 +15,22 @@ import * as contractActions from "../../Core/Contract/Contract.actions";
 import {Container, Page, PageHeading, Toggle} from "../../Elements";
 import {ProjectContentLoader, TransactionsList, TransactionFilters, ProjectSetupEmptyState, NoTransactionsEmptyState} from "../../Components";
 
+const DEFAULT_TX_PER_PAGE = 20;
+
 class ProjectTransactionsPage extends Component {
     constructor(props) {
         super(props);
 
+        const {queryPage, queryPerPage, queryFilters} = props;
+
         this.state = {
             loading: true,
             lastFetch: null,
-            page: props.page,
-            perPage: 20,
+            page: queryPage,
+            perPage: queryPerPage,
             projectSetup: false,
             transactions: [],
-            filters: {},
+            filters: queryFilters,
         };
     }
 
@@ -122,11 +126,48 @@ class ProjectTransactionsPage extends Component {
         this.fetchTransactions();
     };
 
+    updateUrlQuery = () => {
+        const {history} = this.props;
+        const {filters, page, perPage} = this.state;
+
+        const queryData = new URLSearchParams();
+
+        if (page !== 1) {
+            queryData.append('page' ,page);
+        }
+
+        if (perPage !== DEFAULT_TX_PER_PAGE) {
+            queryData.append('perPage', perPage);
+        }
+
+        if (filters[TransactionFilterTypes.STATUS] && filters[TransactionFilterTypes.STATUS].value !== "all") {
+            queryData.append('status', filters[TransactionFilterTypes.STATUS].value);
+        }
+
+        if (filters[TransactionFilterTypes.TYPE] && filters[TransactionFilterTypes.TYPE].value !== "all") {
+            queryData.append('type', filters[TransactionFilterTypes.TYPE].value);
+        }
+
+        if (filters[TransactionFilterTypes.NETWORKS] && filters[TransactionFilterTypes.NETWORKS].value) {
+            queryData.append('network', filters[TransactionFilterTypes.NETWORKS].value);
+        }
+
+        if (filters[TransactionFilterTypes.CONTRACTS] && filters[TransactionFilterTypes.CONTRACTS].value && filters[TransactionFilterTypes.CONTRACTS].value.length) {
+            filters[TransactionFilterTypes.CONTRACTS].value.forEach(value => {
+                queryData.append('contracts', value);
+            });
+        }
+
+        history.push(`?${queryData.toString()}`);
+    };
+
     fetchTransactions = _.debounce(async () => {
         const {project, txActions} = this.props;
         const {filters, page, perPage} = this.state;
 
         let actionResponse;
+
+        this.updateUrlQuery();
 
         if (project.type === ProjectTypes.DEMO) {
             actionResponse= await txActions.fetchExampleTransactions();
@@ -227,10 +268,42 @@ const mapStateToProps = (state, ownProps) => {
     const searchParams = new URLSearchParams(search);
 
     const queryPage = parseInt(searchParams.get('page')) || 1;
+    const queryPerPage = parseInt(searchParams.get('perPage')) || DEFAULT_TX_PER_PAGE;
+    const queryFilters = {};
+
+    if (searchParams.get('status')) {
+        queryFilters[TransactionFilterTypes.STATUS] = {
+            type: TransactionFilterTypes.STATUS,
+            value: searchParams.get('status'),
+        };
+    }
+
+    if (searchParams.get('type')) {
+        queryFilters[TransactionFilterTypes.TYPE] = {
+            type: TransactionFilterTypes.TYPE,
+            value: searchParams.get('type'),
+        };
+    }
+
+    if (searchParams.get('network')) {
+        queryFilters[TransactionFilterTypes.NETWORKS] = {
+            type: TransactionFilterTypes.NETWORKS,
+            value: searchParams.get('network'),
+        };
+    }
+
+    if (searchParams.get('contracts')) {
+        queryFilters[TransactionFilterTypes.CONTRACTS] = {
+            type: TransactionFilterTypes.CONTRACTS,
+            value: searchParams.getAll('contracts'),
+        };
+    }
 
     return {
+        queryPage,
+        queryPerPage,
+        queryFilters,
         project: getProject(state, id),
-        page: queryPage,
         contracts: getContractsForProject(state, id),
         contractsLoaded: areProjectContractsLoaded(state, id),
     }
