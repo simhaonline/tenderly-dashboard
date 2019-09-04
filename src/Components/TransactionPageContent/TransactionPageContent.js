e61cd051171d1f7141ef53ceca8b7cf5f3897518
Miljan Tekic <miljan.tekic@gmail.com>
2019-09-04 10:00:40 +0200

    Handle actions and propagate them to the execution component

--- src/Components/TransactionPageContent/TransactionPageContent.js
+++ src/Components/TransactionPageContent/TransactionPageContent.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, {Component, PureComponent} from 'react';
 import PropTypes from 'prop-types';
 
 import {Transaction} from "../../Core/models";
 
 import {TransactionExecution, TransactionGeneralInformation, TransactionStackTrace} from "../index";
 
-const TransactionPageContent = ({transaction, contracts, callTrace, stackTrace, projectId}) => {
-    return (
-        <div className="TransactionPageContent">
-            <TransactionGeneralInformation contracts={contracts} transaction={transaction}/>
-            {!transaction.status && <TransactionStackTrace stackTrace={stackTrace} contracts={contracts}/>}
-            <TransactionExecution projectId={projectId} transaction={transaction} stackTrace={stackTrace} callTrace={callTrace} contracts={contracts}/>
-        </div>
-    )
-};
+class TransactionPageContent extends PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            action: null,
+        };
+    }
+
+    handleNavigationAction = (action) => {
+        this.setState({
+            action,
+        });
+    };
+
+    render() {
+        const {transaction, contracts, callTrace, stackTrace, projectId} = this.props;
+        const {action} = this.state;
+
+        return (
+            <div className="TransactionPageContent">
+                <TransactionGeneralInformation contracts={contracts} transaction={transaction}/>
+                {!transaction.status && <TransactionStackTrace onNavigate={this.handleNavigationAction} stackTrace={stackTrace} contracts={contracts}/>}
+                <TransactionExecution projectId={projectId} action={action} transaction={transaction} stackTrace={stackTrace} callTrace={callTrace} contracts={contracts}/>
+            </div>
+        )
+    }
+}
 
 TransactionPageContent.propTypes = {
     transaction: PropTypes.instanceOf(Transaction).isRequired,
