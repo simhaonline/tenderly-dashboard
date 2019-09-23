5133819178c7d3083d20895053a8a40d7117c461
Miljan Tekic <miljan.tekic@gmail.com>
2019-09-23 13:42:57 +0200

    Finished parsing for simple alert type

--- src/Utils/AlertHelpers.js
+++ src/Utils/AlertHelpers.js
@@ -1,4 +1,6 @@
-import {SimpleAlertRuleTypes} from "../Common/constants";
+import * as _ from "lodash";
+
+import {AlertRuleExpressionParameterTypes, AlertRuleExpressionTypes, SimpleAlertRuleTypes} from "../Common/constants";
 
 /**
  * @param {AlertRuleExpression[]} expressions
@@ -9,5 +11,52 @@ export function getSimpleRuleType(expressions) {
         return SimpleAlertRuleTypes.UNSET;
     }
 
-    return SimpleAlertRuleTypes.ADVANCED;
+    let advanced = false;
+    let recognizedType;
+
+    if (_.find(expressions, {
+        type: AlertRuleExpressionTypes.TRANSACTION_STATUS,
+        parameters: {
+            [AlertRuleExpressionParameterTypes.TRANSACTION_SUCCESS]: true,
+        },
+    })) {
+        recognizedType = SimpleAlertRuleTypes.SUCCESSFUL_TX;
+    } else if (_.find(expressions, {
+        type: AlertRuleExpressionTypes.TRANSACTION_STATUS,
+        parameters: {
+            [AlertRuleExpressionParameterTypes.TRANSACTION_SUCCESS]: false,
+        },
+    })) {
+        recognizedType = SimpleAlertRuleTypes.FAILED_TX;
+    }
+
+    if (_.find(expressions, {
+        type: AlertRuleExpressionTypes.LOG_EMITTED,
+    })) {
+        advanced = !!recognizedType;
+        recognizedType = SimpleAlertRuleTypes.LOG_EMITTED;
+    }
+
+    if (_.find(expressions, {
+        type: AlertRuleExpressionTypes.METHOD_CALL,
+    })) {
+        advanced = !!recognizedType;
+        recognizedType = SimpleAlertRuleTypes.FUNCTION_CALLED;
+    }
+
+    if (_.find(expressions, {
+        type: AlertRuleExpressionTypes.WHITELISTED_CALLER_ADDRESSES,
+    })) {
+        advanced = !!recognizedType;
+        recognizedType = SimpleAlertRuleTypes.WHITELISTED_CALLERS;
+    }
+
+    if (_.find(expressions, {
+        type: AlertRuleExpressionTypes.BLACKLISTED_CALLER_ADDRESSES,
+    })) {
+        advanced = !!recognizedType;
+        recognizedType = SimpleAlertRuleTypes.BLACKLISTED_CALLERS;
+    }
+
+    return advanced ? SimpleAlertRuleTypes.ADVANCED : recognizedType;
 }
