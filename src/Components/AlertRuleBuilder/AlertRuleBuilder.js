e42a3d68b9a74790bfa3e2cc6b4007f41dcf3793
Miljan Tekic <miljan.tekic@gmail.com>
2019-09-24 17:41:34 +0200

    Started working on the unified alert builder

--- src/Components/AlertRuleBuilder/AlertRuleBuilder.js
+++ src/Components/AlertRuleBuilder/AlertRuleBuilder.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
-import {AlertRule} from "../../Core/models";
+import {AlertRule, Contract, NotificationDestination} from "../../Core/models";
+
+import {Button, Icon} from "../../Elements";
+
+import './AlertRuleBuilder.scss';
 
 const AlertRuleBuilderSteps = {
+    GENERAL: 'general',
     TYPE: 'type',
     TARGET: 'target',
     PARAMETERS: 'parameters',
@@ -14,14 +20,91 @@ class AlertRuleBuilder extends Component {
     constructor(props) {
         super(props);
 
-        // @TODO handle initial rule
-        this.state = {};
+        const {initialStep, initialRule, skipGeneral} = props;
+
+        console.log(initialRule);
+
+        this.state = {
+            step: props.initialStep,
+            steps: {
+                [AlertRuleBuilderSteps.GENERAL]: {
+                    label: 'General Information',
+                    description: 'Some description for this tep',
+                    slug: AlertRuleBuilderSteps.GENERAL,
+                    enabled: !skipGeneral,
+                    completed: false,
+                },
+                [AlertRuleBuilderSteps.TYPE]: {
+                    label: 'Alert Type',
+                    description: 'Some description for this tep',
+                    slug: AlertRuleBuilderSteps.TYPE,
+                    enabled: true,
+                    completed: false,
+                },
+                [AlertRuleBuilderSteps.TARGET]: {
+                    label: 'Alert Target',
+                    description: 'Some description for this tep',
+                    slug: AlertRuleBuilderSteps.TARGET,
+                    enabled: true,
+                    completed: false,
+                },
+                [AlertRuleBuilderSteps.PARAMETERS]: {
+                    label: 'Parameters',
+                    description: 'Some description for this tep',
+                    slug: AlertRuleBuilderSteps.PARAMETERS,
+                    enabled: false,
+                    completed: false,
+                },
+                [AlertRuleBuilderSteps.DESTINATIONS]: {
+                    label: 'Destinations',
+                    description: 'Some description for this tep',
+                    slug: AlertRuleBuilderSteps.DESTINATIONS,
+                    enabled: true,
+                    completed: false,
+                },
+            }
+        };
     }
 
     render() {
+        const {submitButtonLabel} = this.props;
+        const {step: activeStep, steps} = this.state;
+
         return (
             <div className="AlertRuleBuilder">
-                alert rule builder
+                {Object.values(steps).filter(step => step.enabled).map((step, index) => <div key={step.slug}>
+                    <div className={classNames(
+                        "AlertRuleBuilder__StepHeader",
+                        {
+                            "AlertRuleBuilder__StepHeader--Finished": step.complete,
+                        },
+                    )}>
+                        <div className={classNames(
+                            "AlertRuleBuilder__StepIcon",
+                            {
+                                "AlertRuleBuilder__StepIcon--Finished": step.completed,
+                            },
+                        )}>
+                            {!step.completed && <span>{index + 1}</span>}
+                            {step.completed && <Icon icon="check"/>}
+                        </div>
+                        <div className="AlertRuleBuilder__StepInfo">
+                            <h5 className="AlertRuleBuilder__StepInfo__Heading">{step.label}</h5>
+                            <p className="AlertRuleBuilder__StepInfo__Description">{step.description}</p>
+                        </div>
+                    </div>
+                    {step.slug === activeStep && <div className="SimpleAlertRuleStep__Body">
+                        <div className="SimpleAlertRuleStep__Body__Divider"/>
+                        <div className="SimpleAlertRuleStep__Body__Content">
+                            qwe
+                        </div>
+                    </div>}
+                </div>)}
+                <div className="MarginTop4">
+                    <Button>
+                        <span>{submitButtonLabel}</span>
+                    </Button>
+                </div>
             </div>
         );
     }
@@ -29,7 +112,16 @@ class AlertRuleBuilder extends Component {
 
 AlertRuleBuilder.propTypes = {
     initialRule: PropTypes.instanceOf(AlertRule),
-    step: PropTypes.oneOf(Object.values(AlertRuleBuilderSteps)),
+    skipGeneral: PropTypes.bool,
+    submitButtonLabel: PropTypes.string,
+    contracts: PropTypes.arrayOf(PropTypes.instanceOf(Contract)).isRequired,
+    destinations: PropTypes.arrayOf(PropTypes.instanceOf(NotificationDestination)).isRequired,
+    initialStep: PropTypes.oneOf([...Object.values(AlertRuleBuilderSteps), null]),
+};
+
+AlertRuleBuilder.defaultProps = {
+    skipGeneral: false,
+    submitButtonLabel: 'Save',
 };
 
 export default AlertRuleBuilder;
