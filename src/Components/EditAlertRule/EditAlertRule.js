0f14b1a81296f335ef34bbd547c74a8a47596ff8
Miljan Tekic <miljan.tekic@gmail.com>
2019-09-24 13:13:24 +0200

    Started working on the edit alert page

--- src/Components/EditAlertRule/EditAlertRule.js
+++ src/Components/EditAlertRule/EditAlertRule.js
@@ -1,21 +1,102 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import {bindActionCreators} from "redux";
+import {connect} from "react-redux";
 
-import {AlertRuleBuilder} from "..";
+import {getAlertRule, isAlertRuleLoaded} from "../../Common/Selectors/AlertingSelectors";
+import {areProjectContractsLoaded, getProject} from "../../Common/Selectors/ProjectSelectors";
+import {getContractsForProject} from "../../Common/Selectors/ContractSelectors";
+import {
+    areNotificationDestinationsLoaded,
+    getNotificationDestinationsForRule
+} from "../../Common/Selectors/NotificationSelectors";
+
+import * as alertingActions from "../../Core/Alerting/Alerting.actions";
+import * as notificationActions from "../../Core/Notification/Notification.actions";
+import * as contractActions from "../../Core/Contract/Contract.actions";
+
+import {Panel, PanelHeader, PanelContent} from "../../Elements";
+import {AlertRuleBuilder, SimpleLoader} from "..";
 
 class EditAlertRule extends Component {
+    async componentDidMount() {
+        const {ruleId, projectId, isRuleLoaded, actions, notificationActions, contractActions, destinationsLoaded, areContractsLoaded} = this.props;
+
+
+        if (!destinationsLoaded) {
+            notificationActions.fetchNotificationDestinations();
+        }
+
+        if (!isRuleLoaded) {
+            const response = await actions.fetchAlertRuleForProject(projectId, ruleId);
+
+            if (!response.success) {
+                this.setState({
+                    errorFetching: true,
+                });
+            }
+        }
+
+        if (!areContractsLoaded) {
+            contractActions.fetchContractsForProject(projectId);
+        }
+    }
+
     render() {
+        const {rule, contracts, destinations, areContractsLoaded, destinationsLoaded, isRuleLoaded, initialTab} = this.props;
+
+        const pageLoaded = areContractsLoaded && destinationsLoaded && isRuleLoaded;
+
         return (
-            <div>
-                edit alert rule
-                <AlertRuleBuilder/>
-            </div>
+            <Panel>
+                <PanelHeader>
+                    <h3>Edit Alert</h3>
+                </PanelHeader>
+                <PanelContent>
+                    {!pageLoaded && <div className="DisplayFlex Padding4 AlignItemsCenter JustifyContentCenter">
+                        <SimpleLoader/>
+                    </div>}
+                    {pageLoaded && <div>
+                        edit alert rule
+                        <AlertRuleBuilder step={initialTab} rule={rule} contracts={contracts} destinations={destinations}/>
+                    </div>}
+                </PanelContent>
+            </Panel>
         );
     }
 }
 
-EditAlertRule.propTypes = {
+const mapStateToProps = (state, ownProps) => {
+    const {match: {params: {ruleId, projectId}}, location: {search}} = ownProps;
 
+    const searchParams = new URLSearchParams(search);
+
+    const initialTab = searchParams.get('tab');
+
+    const rule = getAlertRule(state, ruleId);
+
+    return {
+        projectId,
+        project: getProject(state, projectId),
+        contracts: getContractsForProject(state, projectId),
+        areContractsLoaded: areProjectContractsLoaded(state, projectId),
+        ruleId,
+        rule,
+        initialTab,
+        isRuleLoaded: isAlertRuleLoaded(state, ruleId),
+        destinations: getNotificationDestinationsForRule(state, rule),
+        destinationsLoaded: areNotificationDestinationsLoaded(state),
+    }
 };
 
-export default EditAlertRule;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        actions: bindActionCreators(alertingActions, dispatch),
+        notificationActions: bindActionCreators(notificationActions, dispatch),
+        contractActions: bindActionCreators(contractActions, dispatch),
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(EditAlertRule);
