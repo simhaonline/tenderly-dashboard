9bcf771692815e1249ac0b7c21c36f8d234f15c5
Miljan Tekic <miljan.tekic@gmail.com>
2019-09-27 13:29:47 +0200

    Basic function and event selection

--- src/Components/AlertRuleBuilder/AlertRuleBuilderParameters.js
+++ src/Components/AlertRuleBuilder/AlertRuleBuilderParameters.js
@@ -4,16 +4,24 @@ import {SimpleAlertRuleTargetTypes, SimpleAlertRuleTypes} from "../../Common/con
 
 import {simpleAlertTypeRequiresContract} from "../../Utils/AlertHelpers";
 
+import {Select} from "../../Elements";
 import {SimpleLoader} from "..";
+
 import AlertRuleBuilderStep from "./AlertRuleBuilderStep";
 
-const FunctionBuilder = ({value, options}) => {
-    return (
-        <div>func</div>
-    )
-};
+class OptionBuilder extends PureComponent {
+    render() {
+        const {value, options, onChange} = this.props;
 
-class FunctionParameterBuilder extends PureComponent {
+        return (
+            <div>
+                <Select value={value} options={options} onChange={onChange} getOptionLabel={option => option.name} getOptionValue={option => option.id}/>
+            </div>
+        );
+    }
+}
+
+class OptionParameterBuilder extends PureComponent {
     render() {
         return (
             <div>
@@ -27,30 +35,30 @@ class AlertRuleBuilderParameters extends PureComponent {
     isStepCompleted = () => {
         const {value} = this.props;
 
-        return !!value && !!value.data;
+        return !!value;
     };
 
     getStepDescription = () => {
-        const {value, alertTarget} = this.props;
+        const {value, alertTarget, alertType} = this.props;
 
-        const type = value ? value.type : null;
+        const type = value ? alertType : null;
 
         switch (type) {
             case SimpleAlertRuleTypes.LOG_EMITTED:
-                return `Event / Log ${value.data.name} is emitted in ${alertTarget ? alertTarget.data.name : ''}`;
+                return `Event / Log ${value.name} is emitted in ${alertTarget ? alertTarget.data.name : ''}`;
             case SimpleAlertRuleTypes.FUNCTION_CALLED:
-                return `Function ${value.data.name}() is called in ${alertTarget ? alertTarget.data.name : ''}`;
+                return `Function ${value.name}() is called in ${alertTarget ? alertTarget.data.name : ''}`;
             default:
                 return 'Set alert trigger parameters.';
         }
     };
 
     render() {
-        const {alertType, alertTarget, onToggle, number, isActiveStep, loading, value, options} = this.props;
+        const {alertType, alertTarget, onToggle, number, isActiveStep, loading, value, options, onChange} = this.props;
 
         const requiresContract = simpleAlertTypeRequiresContract(alertType);
 
-        console.log(loading, value, options);
+        const loaded = loading === false;
 
         const isContractTarget = !(!alertTarget || !alertTarget.data || alertTarget.type !== SimpleAlertRuleTargetTypes.CONTRACT);
 
@@ -61,11 +69,13 @@ class AlertRuleBuilderParameters extends PureComponent {
                     {!isContractTarget && <div>
                         Please select a contract first
                     </div>}
-                    {isContractTarget && loading && <div>
+                    {isContractTarget && !loaded && <div>
                         <SimpleLoader/>
                     </div>}
-                    {isContractTarget && !loading && <div>
-                        content loaded
+                    {isContractTarget && loaded && <div>
+                        {[SimpleAlertRuleTypes.LOG_EMITTED, SimpleAlertRuleTypes.FUNCTION_CALLED].includes(alertType) &&
+                            <OptionBuilder value={value} options={options} onChange={option => onChange(alertType, option)}/>
+                        }
                     </div>}
                 </div>}
                 {!requiresContract && <div>
