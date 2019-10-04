e3e6d222dd01d50215b0dcc81293504826bbe3d4
Miljan Tekic <miljan.tekic@gmail.com>
2019-10-04 14:24:47 +0200

    Fetch collaborators api action and constructor

--- src/Core/Collaboration/Collaboration.actions.js
+++ src/Core/Collaboration/Collaboration.actions.js
@@ -1,4 +1,7 @@
+import {Api} from "../../Utils/Api";
+
 import {ErrorActionResponse, SuccessActionResponse} from "../../Common";
+import {Collaborator} from "../models";
 
 export const FETCH_COLLABORATORS_FOR_PROJECT_ACTION = 'FETCH_COLLABORATORS_FOR_PROJECT';
 
@@ -9,7 +12,15 @@ export const FETCH_COLLABORATORS_FOR_PROJECT_ACTION = 'FETCH_COLLABORATORS_FOR_P
 export const fetchCollaboratorsForProject = (project) => {
     return async dispatch => {
         try {
-            const collaborators = [];
+            const {data} = await Api.get(`/account/${project.owner}/project/${project.slug}/collaborate/users`);
+
+            if (!data || !data.users) {
+                return new ErrorActionResponse();
+            }
+
+            console.log(data);
+
+            const collaborators = data.users.map(user => Collaborator.buildFromResponse(user, project.id));
 
             dispatch({
                 type: FETCH_COLLABORATORS_FOR_PROJECT_ACTION,
--- src/Core/Collaboration/Collaborator.model.js
+++ src/Core/Collaboration/Collaborator.model.js
@@ -1,12 +1,29 @@
 import User from "../Auth/User.model";
 
 class Collaborator extends User {
+    /**
+     * @param {Object} data
+     */
     constructor(data) {
         super(data);
 
         this.projectId = data.projectId;
     }
 
+    /**
+     * @param {Object} response
+     * @param {Project.id} projectId
+     *
+     * @return {Collaborator}
+     */
+    static buildFromResponse(response, projectId) {
+        console.log(response);
+
+        return new Collaborator({
+            projectId,
+        });
+    }
 }
+
 
 export default Collaborator;
