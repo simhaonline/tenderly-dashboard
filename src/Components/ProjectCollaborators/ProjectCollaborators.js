ad11fb40860e39aba0e452dbb530976b682f3cc4
Miljan Tekic <miljan.tekic@gmail.com>
2019-10-04 13:16:07 +0200

    Dummy data

--- src/Components/ProjectCollaborators/ProjectCollaborators.js
+++ src/Components/ProjectCollaborators/ProjectCollaborators.js
@@ -1,16 +1,37 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
+import {Collaborator, Project} from "../../Core/models";
+
+import {Panel, PanelHeader, PanelContent, List, ListItem} from "../../Elements";
+
 class ProjectCollaborators extends Component {
     render() {
         return (
             <div>
-                ProjectCollaborators
+                <Panel>
+                    <PanelHeader>
+                        <h3>qwe</h3>
+                    </PanelHeader>
+                    <PanelContent>
+                        <List>
+                            <ListItem>
+                                Collab1
+                            </ListItem>
+                            <ListItem>
+                                Collab1
+                            </ListItem>
+                        </List>
+                    </PanelContent>
+                </Panel>
             </div>
         );
     }
 }
 
-ProjectCollaborators.propTypes = {};
+ProjectCollaborators.propTypes = {
+    project: PropTypes.instanceOf(Project).isRequired,
+    collaborators: PropTypes.arrayOf(PropTypes.instanceOf(Collaborator)).isRequired,
+};
 
 export default ProjectCollaborators;
