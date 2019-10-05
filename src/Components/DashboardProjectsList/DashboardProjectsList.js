0f6aef09801f0ad0e829d49337202cd93e042be8
Miljan Tekic <miljan.tekic@gmail.com>
2019-10-05 15:04:32 +0200

    Started work on refactoring the project list

--- src/Components/DashboardProjectsList/DashboardProjectsList.js
+++ src/Components/DashboardProjectsList/DashboardProjectsList.js
@@ -11,82 +11,41 @@ import {SimpleLoader, NoProjectsEmptyState, ProjectSetupGuide, FeatureFlag} from
 
 import './DashboardProjectsList.scss';
 
+const DashboardProjectListItem = ({project}) => {
+    return <Link to={`/${project.owner}/${project.slug}`} className="ProjectListItem" key={project.id}>
+        <Card className="ProjectListItemCard">
+            <div className="ProjectNameWrapper">
+                <div className="ProjectName">
+                    {project.type === ProjectTypes.DEMO && <span className="DemoTag">Demo</span>}
+                    <span>{project.name}</span>
+                </div>
+                <div className="ProjectId">
+                    <span>{project.getDisplaySlug()}</span>
+                </div>
+            </div>
+            <div className="ProjectContractInfo">
+                {!!project.lastPushAt && <div className="LastDeployInfo">
+                    Last Push: <span>{moment(project.lastPushAt).fromNow()}</span>
+                </div>}
+                {!project.lastPushAt && <div onClick={event => event.preventDefault()}>
+                    <ProjectSetupGuide label="Add Contracts" project={project} size="small" color="secondary"/>
+                </div>}
+            </div>
+        </Card>
+    </Link>
+};
+
 /**
  * @param {Project[]} projects
  * @param loaded
  * @param onTryExample
  */
 const DashboardProjectsList = ({projects, loaded, onTryExample = () => {}}) => {
-    const data = [
-        {name: 'Page A', transactions: 1,},
-        {name: 'Page B', transactions: 2,},
-        {name: 'Page C', transactions: 0,},
-        {name: 'Page D', transactions: 0,},
-        {name: 'Page E', transactions: 5,},
-        {name: 'Page F', transactions: 11,},
-        {name: 'Page G', transactions: 17,},
-        {name: 'Page G', transactions: 2,},
-        {name: 'Page G', transactions: 3,},
-        {name: 'Page G', transactions: 1,},
-        {name: 'Page G', transactions: 2,},
-        {name: 'Page G', transactions: 4,},
-        {name: 'Page G', transactions: 27,},
-        {name: 'Page G', transactions: 0,},
-        {name: 'Page G', transactions: 2,},
-    ];
-
     return (
         <div className="DashboardProjectsList">
             {(loaded && projects.length === 0) && <NoProjectsEmptyState onTryExample={onTryExample}/>}
             {(loaded && projects.length !== 0) && <div className="ProjectList">
-                {_.sortBy(projects, 'createdAt').map(project =>
-                    <Link to={`/${project.owner}/${project.slug}`} className="ProjectListItem" key={project.id}>
-                        <Card className="ProjectListItemCard">
-                            <div className="ProjectNameWrapper">
-                                <div className="ProjectName">
-                                    {project.type === ProjectTypes.DEMO && <span className="DemoTag">Demo</span>}
-                                    <span>{project.name}</span>
-                                </div>
-                                <div className="ProjectId">
-                                    <span>{project.getDisplaySlug()}</span>
-                                </div>
-                            </div>
-                            <FeatureFlag flag={FeatureFlagTypes.COMING_SOON}>
-                                <div className="ProjectChartWrapper">
-                                    {!!project.lastPushAt && <AreaChart width={250} height={90} data={data} margin={{top: 0, bottom:0,left:0,right:0}}>
-                                        <defs>
-                                            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                                                <stop offset="15%" stopColor="#0076FF" stopOpacity={0.75}/>
-                                                <stop offset="100%" stopColor="#0076FF" stopOpacity={0}/>
-                                            </linearGradient>
-                                        </defs>
-                                        <Tooltip cursor={{ stroke: 'rgba(0,118,255,0.25)', strokeWidth: 1 }} contentStyle={{
-                                            backgroundColor: 'rgba(8,20,33,0.8)',
-                                            border: 'none',
-                                            borderRadius: '4px',
-                                        }}/>
-                                        <YAxis type="number" domain={[0,  dataMax => Math.max(dataMax, 8)]} hide/>
-                                        <Area type="monotone" xAxisId="name" dataKey="transactions" stroke="#0076FF" fill="url(#colorUv)" />
-                                    </AreaChart>}
-                                    {!project.lastPushAt && <div className="ProjectNotSetupState">
-                                        <Icon icon="cloud-off"/>
-                                        <span>Not contracts pushed yet</span>
-                                    </div>}
-                                </div>
-                            </FeatureFlag>
-                            <FeatureFlag flag={FeatureFlagTypes.COMING_SOON} reverse>
-                                <div className="ProjectContractInfo">
-                                    {!!project.lastPushAt && <div className="LastDeployInfo">
-                                        Last Push: <span>{moment(project.lastPushAt).fromNow()}</span>
-                                    </div>}
-                                    {!project.lastPushAt && <div onClick={event => event.preventDefault()}>
-                                        <ProjectSetupGuide label="Add Contracts" project={project} size="small" color="secondary"/>
-                                    </div>}
-                                </div>
-                            </FeatureFlag>
-                        </Card>
-                    </Link>
-                )}
+                {_.sortBy(projects, 'createdAt').map(project => <DashboardProjectListItem key={project.id} project={project}/>)}
                 <Link to={`/project/create`} className="ProjectListItem CreateProjectItem">
                     <Card className="ProjectListItemCard">
                         <Icon icon="plus" className="CreateIcon"/>
