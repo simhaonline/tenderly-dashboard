9aa926b4fdcd77caeb76e085337696c2e8a177cb
Miljan Tekic <miljan.tekic@gmail.com>
2019-10-10 16:47:40 +0200

    Preview for analytics

--- src/Components/AnalyticsWidget/AnalyticsWidget.js
+++ src/Components/AnalyticsWidget/AnalyticsWidget.js
@@ -1,9 +1,15 @@
 import React, {Component, Fragment} from 'react';
 import classNames from 'classnames';
 import moment from "moment";
+import _ from 'lodash';
 import {Area, AreaChart, Line, CartesianGrid, LineChart, Bar, BarChart, YAxis, ResponsiveContainer, Tooltip as RechartsTooltip} from "recharts";
 
-import {AnalyticsWidgetDataRangeTypes, AnalyticsWidgetSizeTypes, AnalyticsWidgetTypes} from "../../Common/constants";
+import {
+    AnalyticsWidgetDataRangeTypes,
+    AnalyticsWidgetListTypeColumnTypes,
+    AnalyticsWidgetSizeTypes,
+    AnalyticsWidgetTypes
+} from "../../Common/constants";
 
 import {Panel, Tag, Icon, Tooltip} from "../../Elements";
 
@@ -33,6 +39,22 @@ class AnalyticsWidget extends Component {
     render() {
         const {widget} = this.props;
 
+        let dataMetadata;
+
+        if ([AnalyticsWidgetTypes.LIST].includes(widget.type)) {
+            dataMetadata = widget.dataPoints.reduce((data, dataPoint) => {
+                if (dataPoint.type === AnalyticsWidgetListTypeColumnTypes.BAR) {
+                    data[dataPoint.key] = {
+                        max: _.maxBy(widget.data, dataPoint.key)[dataPoint.key],
+                        min: _.minBy(widget.data, dataPoint.key)[dataPoint.key],
+                    }
+                }
+                return data;
+            }, {});
+        }
+
+        console.log(dataMetadata);
+
         return (
             <div className={classNames(
                 "AnalyticsWidget",
@@ -40,17 +62,9 @@ class AnalyticsWidget extends Component {
             )}>
                 <Panel className="AnalyticsWidget__Panel">
                     <div className="AnalyticsWidget__Header">
-                        <div>
-                            <div>{widget.name}</div>
-                            <div>
-                                {widget.dataRange === AnalyticsWidgetDataRangeTypes.LAST_7_DAYS && <span>Last 7 Days</span>}
-                                {widget.dataRange === AnalyticsWidgetDataRangeTypes.LAST_14_DAYS && <span>Last 14 Days</span>}
-                                {widget.dataRange === AnalyticsWidgetDataRangeTypes.LAST_30_DAYS && <span>Last 30 Days</span>}
-                                {widget.dataRange === AnalyticsWidgetDataRangeTypes.LAST_WEEK && <span>Last Week</span>}
-                            </div>
-                        </div>
-                        <div>
-                            <div>
+                        <div className="AnalyticsWidget__Header__MainInfo">
+                            <div className="AnalyticsWidget__Header__WidgetName">{widget.name}</div>
+                            <div className="MarginLeftAuto DisplayFlex AlignItemsCenter">
                                 {(widget.alerts && widget.alerts.length > 0) && <Fragment>
                                     <Tag color="primary-outline" size="small" id={`alerts-widget-${widget.id}`}>
                                         <Icon icon="bell"/>
@@ -60,10 +74,24 @@ class AnalyticsWidget extends Component {
                                         <span>This metric has {widget.alerts.length} alerts that are based on it.</span>
                                     </Tooltip>
                                 </Fragment>}
+                                <div className="Padding1">
+                                    <Icon icon="more-vertical"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="AnalyticsWidget__Header__SubInfo">
+                            <div>
+                                {widget.dataRange === AnalyticsWidgetDataRangeTypes.LAST_7_DAYS && <span>Last 7 Days</span>}
+                                {widget.dataRange === AnalyticsWidgetDataRangeTypes.LAST_14_DAYS && <span>Last 14 Days</span>}
+                                {widget.dataRange === AnalyticsWidgetDataRangeTypes.LAST_30_DAYS && <span>Last 30 Days</span>}
+                                {widget.dataRange === AnalyticsWidgetDataRangeTypes.LAST_WEEK && <span>Last Week</span>}
                             </div>
                         </div>
                     </div>
-                    <div className="AnalyticsWidget__Data">
+                    <div className={classNames(
+                        "AnalyticsWidget__Data",
+                        `AnalyticsWidget__Data--${widget.type}`,
+                    )}>
                         {widget.type === AnalyticsWidgetTypes.STACKED_CHART && <Fragment>
                             <ResponsiveContainer>
                                 <AreaChart data={widget.data} margin={{ top: 0, right: 0, bottom: 0, left: 0 }}>
@@ -85,32 +113,44 @@ class AnalyticsWidget extends Component {
                         {widget.type === AnalyticsWidgetTypes.LINE_CHART && <Fragment>
                             <ResponsiveContainer>
                                 <LineChart data={widget.data} margin={{ top: 0, right: 0, bottom: 0, left: 0 }}>
-                                    <RechartsTooltip content={<AnalyticsWidgetTooltip/>} position={{x: 'auto', y: 150,}}/>
+                                    <RechartsTooltip content={<AnalyticsWidgetTooltip/>} cursor={{stroke: '#060e18'}} position={{y: 150,}}/>
                                     {widget.dataPoints.map(point =>
-                                        <Line type="monotone" dataKey={point.key} name={point.name || point.key} stroke={point.color} key={point.key}/>
+                                        <Line type="monotone" dot={{fill: '#133153'}} dataKey={point.key} name={point.name || point.key} stroke={point.color} key={point.key}/>
                                     )}
                                 </LineChart>
                             </ResponsiveContainer>
                         </Fragment>}
                         {widget.type === AnalyticsWidgetTypes.LIST && <Fragment>
-                            <div className="DisplayFlex">
-                                {widget.dataPoints.map(point => <div key={point.key} className="Padding1">
+                            <div className="AnalyticsWidget__Data__ListHeader">
+                                {widget.dataPoints.map(point => <div key={point.key} style={{flex: `${point.size} ${point.size} 0px`}} className="AnalyticsWidget__Data__ListColumn">
                                     {point.name}
                                 </div>)}
                             </div>
-                            <div className="OverflowYScroll">
-                                {widget.data.map((datum, index) => <div key={index} className="DisplayFlex">
-                                    {widget.dataPoints.map(point => <div key={point.key} className="Padding1">
-                                        {datum[point.key]}
+                            <div className="AnalyticsWidget__Data__ListItems">
+                                {widget.data.map((datum, index) => <div key={index} className="AnalyticsWidget__Data__ListItem">
+                                    {widget.dataPoints.map(point => <div key={point.key} style={{flex: `${point.size} ${point.size} 0px`}} className={classNames(
+                                        "AnalyticsWidget__Data__ListColumn",
+                                        `AnalyticsWidget__Data__ListColumn--${point.type}`,
+                                    )}>
+                                        {point.type === AnalyticsWidgetListTypeColumnTypes.VALUE && <span className="LinkText MonospaceFont">{datum[point.key]}</span>}
+                                        {point.type === AnalyticsWidgetListTypeColumnTypes.COUNT && <span>{datum[point.key]}</span>}
+                                        {point.type === AnalyticsWidgetListTypeColumnTypes.PERCENTAGE && <span className="MonospaceFont">{datum[point.key]}%</span>}
+                                        {point.type === AnalyticsWidgetListTypeColumnTypes.BAR && <div className="AnalyticsWidget__Data__BarValue">
+                                            <div className="AnalyticsWidget__Data__Bar" style={{
+                                                width: `${datum[point.key] / dataMetadata[point.key].max * 100}%`,
+                                            }}>
+                                                {datum[point.key]}%
+                                            </div>
+                                        </div>}
                                     </div>)}
                                 </div>)}
                             </div>
                         </Fragment>}
                         {widget.type === AnalyticsWidgetTypes.BAR_CHART && <Fragment>
                             <ResponsiveContainer>
-                                <BarChart data={widget.data} margin={{ top: 0, right: 0, bottom: 0, left: 0 }}>
-                                    <RechartsTooltip content={<AnalyticsWidgetTooltip/>}/>
-                                    <CartesianGrid vertical={false} strokeDasharray="6" stroke="rgba(255,255,255,0.5)"/>
+                                <BarChart data={widget.data} margin={{ top: 24, right: 0, bottom: 0, left: 0 }}>
+                                    <RechartsTooltip cursor={{fill: 'rgba(15, 39, 67, 0.75)'}} content={<AnalyticsWidgetTooltip/>}/>
+                                    <CartesianGrid vertical={false} strokeDasharray="6" stroke="#040b13"/>
                                     <YAxis orientation="right" tick={{fill: 'white'}} mirror/>
                                     {widget.dataPoints.map(point =>
                                         <Bar type="monotone" dataKey={point.key} name={point.name || point.key} fill={point.color} barSize={20} stroke={point.color} key={point.key}/>
