209d9060ae56e0e706183538945e08b73b51f995
Miljan Tekic <miljan.tekic@gmail.com>
2019-10-18 09:17:03 +0200

    Added pie chart to flame graph

--- src/Components/CallTraceFlameGraph/CallTraceFlameGraph.js
+++ src/Components/CallTraceFlameGraph/CallTraceFlameGraph.js
@@ -1,20 +1,103 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
+import {Pie, PieChart, Tooltip} from "recharts";
 
 import {CallTrace, Transaction} from "../../Core/models";
 
 import {FlameGraph} from "../index";
 
+/**
+ * @param {Trace} parentTrace
+ * @param {Trace} trace
+ */
+const SelectedTraceBreakdown = ({parentTrace, trace}) => {
+    let graphData = [];
+    const graphDataByName = {};
+    let usedGasByCalls = 0;
+
+    if (trace.calls && trace.calls.length > 0) {
+        trace.calls.forEach(traceCall => {
+            graphData.push();
+            const name = traceCall.functionName || `Op: [${traceCall.op}]`;
+            if (!graphDataByName[name]) {
+                graphDataByName[name] = {
+                    name,
+                    gas: 0,
+                    percentage: 0,
+                    count: 0,
+                };
+            }
+
+            graphDataByName[name].gas += traceCall.gasUsed;
+            graphDataByName[name].percentage = graphDataByName[name].gas / trace.gasUsed;
+            graphDataByName[name].count += 1;
+
+            usedGasByCalls += traceCall.gasUsed;
+        });
+    }
+
+    const otherGasUsed = trace.gasUsed - usedGasByCalls;
+
+    graphData = _.orderBy(Object.values(graphDataByName), 'gas', 'desc');
+
+    graphData.push({
+        name: "Other",
+        gas: otherGasUsed,
+        percentage: otherGasUsed / trace.gasUsed,
+    });
+
+    return <div className="DisplayFlex">
+        <div>
+            <PieChart width={150} height={150}>
+                <Pie dataKey="gas" data={graphData} cx={75} cy={75} innerRadius={20} startAngle={450} endAngle={90} isAnimationActive={false} outerRadius={60} fill="#0076FF"/>
+                <Tooltip/>
+            </PieChart>
+        </div>
+        <div className="MarginLeft4">
+            {graphData.map((datum, index) => <div key={index}>
+                <span className="MutedText">{_.round(datum.percentage * 100, 2).toFixed(2)}%</span>
+                <span> - {datum.name}</span>
+                {!!datum.count && datum.count > 1 && <span> [{datum.count}]</span>}
+            </div>)}
+        </div>
+    </div>;
+};
+
 class CallTraceFlameGraph extends Component {
     constructor(props) {
         super(props);
 
-        const graphData = this.computeGraphData(props.callTrace, props.transaction);
+        const {callTrace, transaction} = props;
+
+        const graphData = this.computeGraphData(callTrace, transaction);
+
+        const flatCallTrace = this.flattenCallTrace(callTrace.trace);
 
         this.state = {
             graphData,
+            currentActiveNode: null,
+            flatCallTrace,
         };
     }
+
+    /**
+     * @param {Trace} trace
+     * @return {Object.<string, Trace>}
+     */
+    flattenCallTrace = (trace) => {
+        let data = {
+            [trace.depthId]: trace,
+        };
+
+        if (trace.calls) {
+            trace.calls.forEach(childTrace => {
+                Object.assign(data, this.flattenCallTrace(childTrace));
+            });
+        }
+
+        return data;
+    };
 
     /**
      * @param {Trace[]} traces
@@ -25,14 +108,13 @@ class CallTraceFlameGraph extends Component {
             return null;
         }
 
-        const data = [
-
-        ];
+        const data = [];
 
         traces.forEach(trace => {
             data.push({
                 name: `${trace.functionName || trace.contract} - ${trace.gasUsed} Gas`,
                 value: trace.gasUsed,
+                depthId: trace.depthId,
                 children: this.computeTraceToGraphChildren(trace.calls),
             });
         });
@@ -71,12 +153,26 @@ class CallTraceFlameGraph extends Component {
         }
     };
 
+    handleTraceClick = (item) => {
+        if (item.data && item.data.depthId) {
+            this.setState({
+                currentActiveNode: item,
+            });
+        } else {
+            this.setState({
+                currentActiveNode: null,
+            });
+        }
+    };
+
     render() {
-        const {graphData} = this.state;
+        const {graphData, currentActiveNode: node, flatCallTrace} = this.state;
 
         return (
             <div className="CallTraceFlameGraph">
-                <FlameGraph data={graphData} stretch/>
+                <FlameGraph onClick={this.handleTraceClick} data={graphData} stretch/>
+                {!!node && <SelectedTraceBreakdown parentTrace={flatCallTrace[node.parent.data.depthId]}
+                                                   trace={flatCallTrace[node.data.depthId]}/>}
             </div>
         );
     }
