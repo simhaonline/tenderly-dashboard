0413bd95cb2a0b7e9deb33c183919c2f12b05e68
Miljan Tekic <miljan.tekic@gmail.com>
2019-10-31 11:27:54 +0100

    Redux logic for fetching contracts

--- src/Pages/Project/ProjectAddContractPage.js
+++ src/Pages/Project/ProjectAddContractPage.js
@@ -1,22 +1,69 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
 
-import {Page, Container, PageHeading} from "../../Elements";
-import {CliUsageInstructions, AddPublicContractForm} from "../../Components";
+import {
+    areProjectContractsLoaded,
+    getProjectBySlugAndUsername
+} from "../../Common/Selectors/ProjectSelectors";
+import {getContractsForProject} from "../../Common/Selectors/ContractSelectors";
+
+import {contractActions} from "../../Core/actions";
+
+import {Page, Container, PageHeading, Icon, Button} from "../../Elements";
+import {CliUsageInstructions, AddPublicContractForm, ProjectContentLoader} from "../../Components";
 
 class ProjectAddContractPage extends Component {
+    componentDidMount() {
+        const {actions, project, contractsLoaded} = this.props;
+
+        if (!contractsLoaded) {
+            actions.fetchContractsForProject(project);
+        }
+    }
+
     render() {
+        const {contractsLoaded, project} = this.props;
+
         return (
             <Page>
                 <Container>
                     <PageHeading>
+                        <Button outline to={`/${project.owner}/${project.slug}/collaborators`}>
+                            <Icon icon="arrow-left"/>
+                        </Button>
                         <h1>Add Contract</h1>
                     </PageHeading>
-                    <CliUsageInstructions/>
-                    <AddPublicContractForm/>
+                    {!contractsLoaded && <ProjectContentLoader text="Fetching required data"/>}
+                    {contractsLoaded && <Fragment>
+                        <CliUsageInstructions/>
+                        <AddPublicContractForm/>
+                    </Fragment>}
                 </Container>
             </Page>
         );
     }
 }
 
-export default ProjectAddContractPage;
+const mapStateToProps = (state, ownProps) => {
+    const {match: {params: {slug, username}}} = ownProps;
+
+    const project = getProjectBySlugAndUsername(state, slug, username);
+
+    return {
+        project,
+        contracts: getContractsForProject(state, project.id),
+        contractsLoaded: areProjectContractsLoaded(state, project.id),
+    }
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        actions: bindActionCreators(contractActions, dispatch),
+    }
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(ProjectAddContractPage);
