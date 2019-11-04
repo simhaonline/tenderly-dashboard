2f954daecca9e386c4f6d1b8324efd9a2ab201d0
Miljan Tekic <miljan.tekic@gmail.com>
2019-11-04 16:32:47 +0100

    Accordiong component and basic styling for the component

--- src/Components/CliUsageInstructions/CliUsageInstructions.js
+++ src/Components/CliUsageInstructions/CliUsageInstructions.js
@@ -1,18 +1,35 @@
 import React, {Component} from 'react';
 
-import {Panel, PanelHeader, PanelContent} from "../../Elements";
+import {Panel, PanelHeader, Accordion, PanelContent} from "../../Elements";
 
 import './CliUsageInstructions.scss';
 
 class CliUsageInstructions extends Component {
+    state = {
+        currentStep: '',
+    };
+
+    /**
+     * @param {string} step
+     */
+    setCurrentStep = (step) => {
+        this.setState({
+            currentStep: step,
+        })
+    };
+
     render() {
+        const {currentStep} = this.state;
+
         return (
             <Panel className="CliUsageInstructions">
                 <PanelHeader>
                     <h3>Upload via CLI</h3>
                 </PanelHeader>
                 <PanelContent>
-
+                    <Accordion open={currentStep === 'install_cli'} onToggle={() => this.setCurrentStep('install_cli')}>
+                        This is the first step
+                    </Accordion>
                 </PanelContent>
             </Panel>
         );
---
+++ src/Elements/Accordion/AccordionGroup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AccordionGroup = ({children}) => {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+};
+
+export default AccordionGroup;
