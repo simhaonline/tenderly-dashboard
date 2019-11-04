485ec77eede348b19acc38baef6d7ef69350bde7
Miljan Tekic <miljan.tekic@gmail.com>
2019-11-04 09:59:18 +0100

    Basic styling for add contract method picker

--- src/Components/AddContractMethodPicker/AddContractMethodPicker.js
+++ src/Components/AddContractMethodPicker/AddContractMethodPicker.js
@@ -8,7 +8,7 @@ const AddContractMethods = [
     {
         slug: 'verified',
         icon: 'compass',
-        title: 'Verified Contracts',
+        title: 'Import Verified',
     },
     {
         slug: 'cli',
@@ -19,10 +19,10 @@ const AddContractMethods = [
 
 const AddContractMethodPicker = ({onSelect, currentActive}) => {
     return (
-        <CardsWrapper horizontal className="MarginBottom4">
-            {AddContractMethods.map(method => <Card key={method.slug} className="Upload" selectable onClick={() => onSelect(method.slug)} selected={currentActive === method.slug}>
-                <Icon icon={method.icon}/>
-                <div>{method.title}</div>
+        <CardsWrapper horizontal className="MarginBottom4 AddContractMethodPicker">
+            {AddContractMethods.map(method => <Card key={method.slug} className="AddContractMethodPicker__Method" selectable onClick={() => onSelect(method.slug)} selected={currentActive === method.slug}>
+                <Icon icon={method.icon} className="AddContractMethodPicker__Method__Icon"/>
+                <div className="AddContractMethodPicker__Method__Title">{method.title}</div>
             </Card>)}
         </CardsWrapper>
     );
