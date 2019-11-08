9dc42161cb3b43ce0413e7c91c9cd464c8a1831a
Miljan Tekic <miljan.tekic@gmail.com>
2019-11-08 16:30:51 +0100

    Removed unused code

--- src/Pages/PublicContracts/ExplorerPage.js
+++ src/Pages/PublicContracts/ExplorerPage.js
@@ -1,14 +1,7 @@
 import React, {Component} from "react";
-import {bindActionCreators} from "redux";
-import {connect} from "react-redux";
 import {Helmet} from "react-helmet";
 
 import Analytics from "../../Utils/Analytics";
-import {getNetworkForRouteSlug} from "../../Utils/RouterHelpers";
-
-import {getNetworkPublicContractsForPage} from "../../Common/Selectors/PublicContractSelectors";
-
-import * as publicContractsActions from '../../Core/PublicContracts/PublicContracts.actions';
 
 import {Page, Container} from "../../Elements";
 import {PublicNetworksSearch, TenderlyLogo, ExplorerDescription, ExplorerHeader} from "../../Components";
@@ -40,24 +33,4 @@ class ExplorerPage extends Component {
     }
 }
 
-const mapStateToProps = (state, ownProps) => {
-    const {match: {params: {network}}} = ownProps;
-
-    let networkType = getNetworkForRouteSlug(network);
-
-    return {
-        contracts: getNetworkPublicContractsForPage(state, networkType, 0),
-        networkType,
-    };
-};
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        actions: bindActionCreators(publicContractsActions, dispatch),
-    }
-};
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
-)(ExplorerPage);
+export default ExplorerPage;
