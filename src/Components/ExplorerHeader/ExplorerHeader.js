2e53ce8157e10a26ad3f52dbc5994340844e5a37
Miljan Tekic <miljan.tekic@gmail.com>
2019-11-08 16:36:29 +0100

    Session header

--- src/Components/ExplorerHeader/ExplorerHeader.js
+++ src/Components/ExplorerHeader/ExplorerHeader.js
@@ -2,22 +2,38 @@ import React, {Component} from 'react';
 import {Link} from "react-router-dom";
 
 import {Button} from '../../Elements';
+import SessionHeaderMenu from "../SessionHeaderMenu/SessionHeaderMenu";
 
 import './ExplorerHeader.scss';
+import {connect} from "react-redux";
 
 class ExplorerHeader extends Component {
     render() {
+        const {loggedIn} = this.props;
+
         return (
             <div className="ExplorerHeader">
-                <div className="ExplorerHeader__Navigation">
+                {!loggedIn && <div className="ExplorerHeader__Navigation">
                     <Link to="login" className="ExplorerHeader__Navigation__Link">Login</Link>
                     <Button className="ExplorerHeader__Navigation__CtaButton" to="/register">
                         <span>Create Account </span>
                     </Button>
-                </div>
+                </div>}
+                {loggedIn && <div className="ExplorerHeader__SessionMenu">
+                    <SessionHeaderMenu/>
+                </div>}
             </div>
         );
     }
 }
 
-export default ExplorerHeader;
+const mapStateToProps = (state) => {
+    return {
+        loggedIn: state.auth.loggedIn,
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    null,
+)(ExplorerHeader);
