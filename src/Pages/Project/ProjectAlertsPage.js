a9276f90c0a64410df2f351bf349dd2a2bc1a527
Miljan Tekic <miljan.tekic@gmail.com>
2019-11-12 16:28:15 +0100

    Learned how navigations works in UI. Generally..

--- src/Pages/Project/ProjectAlertsPage.js
+++ src/Pages/Project/ProjectAlertsPage.js
@@ -9,9 +9,6 @@ import {getProjectBySlugAndUsername} from "../../Common/Selectors/ProjectSelecto
 
 import {Container, Page, PageHeading, Button} from "../../Elements";
 import {
-    PageSegments,
-    PageSegmentSwitcher,
-    PageSegmentContent,
     ProjectAlertHistory,
     ProjectAlertDestinations,
     ProjectAlertRules,
@@ -19,53 +16,9 @@ import {
     PermissionControl,
 } from "../../Components";
 
-const RULES_TAB = 'rules';
-const HISTORY_TAB = 'history';
-const INTEGRATIONS_TAB = 'destinations';
-
-const PageSegmentsOptions = [
-    {
-        label: 'Alerts',
-        description: 'Setup custom rules and triggers for alerts',
-        value: RULES_TAB,
-    },
-    {
-        label: 'History',
-        description: 'View alerts that were triggered and sent',
-        value: HISTORY_TAB,
-    },
-    {
-        label: 'Destinations',
-        description: 'Set alert destinations like email, slack etc.',
-        value: INTEGRATIONS_TAB,
-    },
-];
-
 class ProjectAlertsPage extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            currentSegment: props.initialTab || RULES_TAB,
-        }
-    }
-
-    /**
-     * @param {String} segment
-     */
-    handleSegmentSwitch = (segment) => {
-        const {project} = this.props;
-
-        this.props.history.push(`/${project.owner}/${project.slug}/alerts/${segment}`);
-
-        this.setState({
-            currentSegment: segment,
-        });
-    };
-
     render() {
         const {project} = this.props;
-        const {currentSegment} = this.state;
 
         return (
             <Page id="ProjectPage">
@@ -81,20 +34,11 @@ class ProjectAlertsPage extends Component {
                         </div>
                     </PageHeading>
                     {!project.isSetup && <ProjectSetupEmptyState project={project}/>}
-                    {project.isSetup && <PageSegments>
-                        <PageSegmentSwitcher current={currentSegment} options={PageSegmentsOptions} onSelect={this.handleSegmentSwitch}/>
-                        <Switch>
-                            <Route path={`/:username/:slug/alerts/rules`} render={() => <PageSegmentContent>
-                                <ProjectAlertRules/>
-                            </PageSegmentContent>}/>
-                            <Route path={`/:username/:slug/alerts/history`} render={() => <PageSegmentContent>
-                                <ProjectAlertHistory project={project}/>
-                            </PageSegmentContent>}/>
-                            <Route path={`/:username/:slug/alerts/destinations`} render={() => <PageSegmentContent>
-                                <ProjectAlertDestinations/>
-                            </PageSegmentContent>}/>
-                        </Switch>
-                    </PageSegments>}
+                    {project.isSetup && <Switch>
+                        <Route path={`/:username/:slug/alerts/rules`} component={ProjectAlertRules}/>
+                        <Route path={`/:username/:slug/alerts/history`} render={() => <ProjectAlertHistory project={project}/>}/>
+                        <Route path={`/:username/:slug/alerts/destinations`} component={ProjectAlertDestinations}/>
+                    </Switch>}
                 </Container>
             </Page>
         )
@@ -102,12 +46,11 @@ class ProjectAlertsPage extends Component {
 }
 
 const mapStateToProps = (state, ownProps) => {
-    const {match: {params: {username, slug, tab}}} = ownProps;
+    const {match: {params: {username, slug}}} = ownProps;
 
     const project = getProjectBySlugAndUsername(state, slug, username);
 
     return {
-        initialTab: tab,
         project,
     }
 };
