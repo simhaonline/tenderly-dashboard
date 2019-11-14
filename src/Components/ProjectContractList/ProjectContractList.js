206ae8a48b7514f69c01321be7c8fecd08e56e83
Miljan Tekic <miljan.tekic@gmail.com>
2019-11-14 12:09:51 +0100

    Refactor the project contract list  view

--- src/Components/ProjectContractList/ProjectContractList.js
+++ src/Components/ProjectContractList/ProjectContractList.js
@@ -1,22 +1,22 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {withRouter} from "react-router-dom";
-
-import {getRouteSlugForNetwork} from "../../Utils/RouterHelpers";
+import Blockies from "react-blockies";
+import {Link, withRouter} from "react-router-dom";
 
 import {Contract, ProjectContract} from "../../Core/models";
 import {NetworkTypes} from "../../Common/constants";
 
-import Table from "../../Elements/Table/Table";
+import {Card, Icon, Input} from "../../Elements";
 import {
     ContractAddressColumn,
     ContractDeployedAtColumn,
     ContractListeningColumn,
     ContractFilesColumn,
-    NetworkColumn
+    NetworkColumn, NetworkTag
 } from "../index";
 
 import './ProjectContractList.scss';
+import {generateShortAddress} from "../../Utils/AddressFormatter";
 
 const projectContractsTableConfiguration = [
     {
@@ -69,6 +69,10 @@ const groupSorting = [
 ];
 
 class ProjectContractList extends Component{
+    state = {
+        searchQuery: '',
+    };
+
     /**
      * @param {Contract} contract
      */
@@ -82,12 +86,52 @@ class ProjectContractList extends Component{
 
     render() {
         const {contracts, projectContracts} = this.props;
+        const {searchQuery} = this.state;
 
         return (
             <div className="ProjectContractList">
-                {projectContracts.map(projectContract => <div key={projectContract.id}>
-                    {projectContract.name}
-                </div>)}
+                {projectContracts.length > 12 && <div className="ProjectContractList__FilterWrapper">
+                    <div className="MaxWidth480">
+                        <Input icon="filter" autoComplete="off" label="Filter contracts" field="searchQuery"  value={searchQuery}/>
+                    </div>
+                </div>}
+                <div className="ProjectContractList__ItemsWrapper">
+                    {projectContracts.map(projectContract => <Link to={projectContract.getUrl()} key={projectContract.id} className="ProjectContractList__Item">
+                        <Card clickable>
+                            <div className="DisplayFlex AlignItemsCenter">
+                                <Blockies size={8} scale={5} className="BorderRadius2" seed={projectContract.contractId}/>
+                                <div className="MarginLeft2">
+                                    <div className="SemiBoldText MarginBottom1">{projectContract.name}</div>
+                                    <div className="LinkText MonospaceFont">{generateShortAddress(projectContract.address, 12, 6)}</div>
+                                </div>
+                            </div>
+                            <div className="MarginTop2">
+                                <NetworkTag size="small" network={projectContract.network}/>
+
+                            </div>
+                            <div className="MarginTop2">{projectContract.revisionsCount} Revisions</div>
+                            <div className="ProjectContractList__Item__AddedByWrapper">
+                                <div className="DisplayFlex AlignItemsCenter">
+                                    <div></div>
+                                    <div className="MarginLeft2">
+                                        <div>Miljan Tekic</div>
+                                        <div>Pushed 5 hours ago</div>
+                                    </div>
+                                    <div className="MarginLeftAuto TextAlignRight">
+                                        {projectContract.tags.length > 0 && <div>
+                                            <Icon icon="tag" className="MarginRight1"/>
+                                            <span>{projectContract.getLatestTag().label}</span>
+                                        </div>}
+                                        <div>
+                                            <span>View Push</span>
+                                            <Icon icon="external-link" className="MarginLeft1"/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </Card>
+                    </Link>)}
+                </div>
             </div>
         )
     }
