9debb4f83f68010e0c7f7031b38a233f7ba4c52d
Miljan Tekic <miljan.tekic@gmail.com>
2019-11-14 16:57:45 +0100

    Finished private networks in reducers

--- src/Core/Network/Network.reducer.js
+++ src/Core/Network/Network.reducer.js
@@ -1,10 +1,15 @@
+import _ from 'lodash';
+
 import {NetworkTypes} from "../../Common/constants";
 
 import Network from "./Network.model";
 
 import {LOG_OUT_ACTION} from "../Auth/Auth.actions";
+import {FETCH_CONTRACTS_FOR_PROJECT_ACTION} from "../Contract/Contract.actions";
+import PrivateNetwork from "./PrivateNetwork.model";
 
 const initialState = {
+    /** @type {Object.<Network.id, Network>} */
     defaultNetworks: {
         ...Object.values(NetworkTypes).reduce((data, network) => {
             data[network] = Network.buildFromNetworkType(network);
@@ -12,11 +17,45 @@ const initialState = {
             return data;
         }, {}),
     },
-    userNetworks: {},
+    /** @type {Object.<Network.id, PrivateNetwork>} */
+    privateNetworks: {},
+    /** @type {Object.<Project.id, Network.id[]>} */
+    projectPrivateNetworksMap: {},
 };
+
+const PRIVATE_NETWORKS_COLOR_MAP = [
+    '#3F51B5',
+    '#E91E63',
+    '#CDDC39',
+    '#2196F3',
+    '#607D8B',
+    '#FFC107',
+];
 
 const NetworkReducer = (state = initialState, action) => {
     switch (action.type) {
+        case FETCH_CONTRACTS_FOR_PROJECT_ACTION:
+            const unknownNetworks = _.uniqBy(action.projectContracts, 'network')
+                .map(pc => pc.network)
+                .filter(n => !state.defaultNetworks[n]);
+
+            const privateNetworks = unknownNetworks.map((networkId, index) => PrivateNetwork.buildFromNetworkId(networkId, action.projectId, PRIVATE_NETWORKS_COLOR_MAP[index % PRIVATE_NETWORKS_COLOR_MAP.length]));
+
+            return {
+                ...state,
+                privateNetworks: {
+                    ...state.privateNetworks,
+                    ...privateNetworks.reduce((data, network) => {
+                        data[network.id] = network;
+
+                        return data;
+                    }, {}),
+                },
+                projectPrivateNetworksMap: {
+                    ...state.projectPrivateNetworksMap,
+                    [action.projectId]: privateNetworks.map(pn => pn.id),
+                },
+            };
         case LOG_OUT_ACTION:
             return initialState;
         default:
--- src/Core/Network/PrivateNetwork.model.js
+++ src/Core/Network/PrivateNetwork.model.js
@@ -4,16 +4,34 @@ class PrivateNetwork extends Network{
     constructor(data) {
         super(data);
 
-        /** @type {string} */
-        this.networkId = data.networkId;
-
         /** @type {Project.id} */
         this.projectId = data.projectId;
+
+        /** @type {string} */
+        this.status = data.status;
 
         /** @type {string} */
         this.color = data.color;
     }
 
+    /**
+     * @param {string} networkId
+     * @param {Project.id} projectId
+     * @param {string} color
+     * @returns {PrivateNetwork}
+     */
+    static buildFromNetworkId(networkId, projectId, color) {
+        return new PrivateNetwork({
+            id: networkId,
+            name: 'Private',
+            shorthand: 'P',
+            apiId: networkId,
+            routeId: networkId,
+            projectId: projectId,
+            status: 'online',
+            color,
+        });
+    }
 }
 
 export default PrivateNetwork;
