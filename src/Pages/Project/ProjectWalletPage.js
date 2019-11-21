a0859bdf4f41ce9c3e42751b00ad82548d743e4d
Miljan Tekic <miljan.tekic@gmail.com>
2019-11-21 10:35:13 +0100

    Single wallet page

--- src/Pages/Project/ProjectWalletPage.js
+++ src/Pages/Project/ProjectWalletPage.js
@@ -1,19 +1,42 @@
-import React, {Component} from 'react';
+import React, {Component, PureComponent} from 'react';
 import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
 
+import {getNetworkForRouteSlug} from "../../Utils/RouterHelpers";
+
 import {
     areProjectWalletsLoaded,
-    getProjectBySlugAndUsername,
-    getProjectWallets
+    getProjectBySlugAndUsername, getProjectWallet,
 } from "../../Common/Selectors/ProjectSelectors";
-import {getWalletsForProject} from "../../Common/Selectors/WalletSelectors";
+import {getWalletByAddressAndNetwork} from "../../Common/Selectors/WalletSelectors";
 
 import {walletActions} from "../../Core/actions";
 
-import {Button, Icon, Page, PageHeading} from "../../Elements";
+import {Button, Container, Icon, Page, PageHeading} from "../../Elements";
+import {ProjectContentLoader} from "../../Components";
 
-class ProjectWalletPage extends Component {
+class ProjectWalletPage extends PureComponent {
+    constructor(props) {
+        super(props);
+
+        const {project, match: {params: {address, network}}} = props;
+
+        const routeBase = `${project.getUrlBase()}/wallet/${network}/${address}`;
+
+        this.state = {
+            tabs: [
+                {
+                    route: `${routeBase}`,
+                    label: 'Transactions',
+                },
+                {
+                    route: `${routeBase}/tokens`,
+                    label: 'Tokens',
+                },
+            ],
+        };
+    }
+
     componentDidMount() {
         const {project, walletActions, walletsLoaded} = this.props;
 
@@ -23,31 +46,47 @@ class ProjectWalletPage extends Component {
     }
 
     render() {
-        const {project} = this.props;
+        const {project, walletsLoaded, projectWallet} = this.props;
+        const {tabs} = this.state;
+
+        if (!walletsLoaded) {
+            return <Page tabs={tabs}>
+                <Container>
+                    <ProjectContentLoader text="Fetching contract..."/>
+                </Container>
+            </Page>;
+        }
 
         return (
-            <Page>
-                <PageHeading>
-                    <Button outline to={`/${project.owner}/${project.slug}/wallets`}>
-                        <Icon icon="arrow-left"/>
-                    </Button>
-                    <h1>Wallet</h1>
-                </PageHeading>
+            <Page tabs={tabs}>
+                <Container>
+                    <PageHeading>
+                        <Button outline to={`/${project.owner}/${project.slug}/wallets`}>
+                            <Icon icon="arrow-left"/>
+                        </Button>
+                        <h1>{projectWallet.name}</h1>
+                    </PageHeading>
+                    <div>
+
+                    </div>
+                </Container>
             </Page>
         );
     }
 }
 
 const mapStateToProps = (state, ownProps) => {
-    const {match: {params: {slug, username}}} = ownProps;
+    const {match: {params: {slug, username, network, address}}} = ownProps;
 
+    const networkType = getNetworkForRouteSlug(network);
     const project = getProjectBySlugAndUsername(state, slug, username);
+    const wallet = getWalletByAddressAndNetwork(state, address, networkType);
 
     return {
         project,
         walletsLoaded: areProjectWalletsLoaded(state, project.id),
-        projectWallets: getProjectWallets(state, project.id),
-        wallets: getWalletsForProject(state, project.id),
+        projectWallet: getProjectWallet(state, project.id, wallet),
+        wallet,
     };
 };
 
