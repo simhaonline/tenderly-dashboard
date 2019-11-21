b518123e8852154a2cb18dc76991b640b1d06772
Miljan Tekic <miljan.tekic@gmail.com>
2019-11-21 13:54:38 +0100

    Started working on copy. Gave up.

--- src/Components/PaidFeatureButton/PaidFeatureButton.js
+++ src/Components/PaidFeatureButton/PaidFeatureButton.js
@@ -1,36 +1,69 @@
-import React, {PureComponent} from 'react';
+import React, {Fragment, PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 
 import {UserPlanTypes} from "../../Common/constants";
 
-import {Button} from "../../Elements";
+import {Button, Dialog, DialogBody} from "../../Elements";
 
 class PaidFeatureButton extends PureComponent {
-    handleOnClick = (event) => {
-        const {onClick} = this.props;
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            hasRequiredPlan: false,
+            upgradeModalOpen: false,
+        };
+    }
+
+
+    handleOnClick = (event) => {
         event.stopPropagation();
         event.preventDefault();
 
-        if (onClick) {
-            onClick(event);
-        }
+        this.setState({
+            upgradeModalOpen: true,
+        });
+    };
+
+    handleModalClose = () => {
+        this.setState({
+            upgradeModalOpen: false,
+        });
     };
 
     render() {
-        const {children, ...props} = this.props;
+        const {children, planRequired, dispatch, ...props} = this.props;
+        const {upgradeModalOpen, hasRequiredPlan} = this.state;
+
+        if (hasRequiredPlan) {
+            return <Button {...props}>
+                {children}
+            </Button>;
+        }
 
         return (
-            <Button {...props} onClick={this.handleOnClick}>
-                {children}
-            </Button>
+            <Fragment>
+                <Button {...props} onClick={this.handleOnClick}>
+                    {children}
+                </Button>
+                <Dialog open={upgradeModalOpen} onClose={this.handleModalClose}>
+                    <DialogBody>
+                        <h2>Pro</h2>
+                        <h3>$250 / mo</h3>
+                        <Button size="large" color="secondary" stretch>
+                            <span className="SemiBoldText">Start Free Trial</span>
+                        </Button>
+                        <div className="FontSize2 MarginLeft2 MarginTop2">* 14-day free Trial. No credit card required.</div>
+                    </DialogBody>
+                </Dialog>
+            </Fragment>
         );
     }
 }
 
 PaidFeatureButton.propTypes = {
-    planRequired: PropTypes.oneOf([Object.values(UserPlanTypes)]),
+    planRequired: PropTypes.oneOf(Object.values(UserPlanTypes)).isRequired,
 };
 
 const mapStateToProps = () => {
--- src/Components/PaidFeatureMessage/PaidFeatureMessage.js
+++ src/Components/PaidFeatureMessage/PaidFeatureMessage.js
@@ -1,13 +1,32 @@
 import React, {Component} from 'react';
 
+import {Button} from "../../Elements";
+import {connect} from "react-redux";
+
 class PaidFeatureMessage extends Component {
     render() {
         return (
-            <div>
-
+            <div className="PaidFeatureMessage">
+                <div>
+                    <div>Sign up for Pro plan trial</div>
+                    <div>No credit card required</div>
+                </div>
+                <div>
+                    <Button>
+                        Start Trial
+                    </Button>
+                </div>
             </div>
         );
     }
 }
 
-export default PaidFeatureMessage;
+const mapStateToProps = (state) => {
+    return {
+
+    };
+};
+
+export default connect(
+    mapStateToProps,
+)(PaidFeatureMessage);
