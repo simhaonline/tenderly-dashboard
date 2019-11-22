6951b8f63aacef89b0d8002d591ada04d0ba153f
Miljan Tekic <miljan.tekic@gmail.com>
2019-11-22 16:40:26 +0100

    Add wallet page improvements

--- src/Pages/Project/ProjectAddWalletPage.js
+++ src/Pages/Project/ProjectAddWalletPage.js
@@ -1,18 +1,37 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
 
 import {
+    areProjectWalletsLoaded,
     getProjectBySlugAndUsername,
 } from "../../Common/Selectors/ProjectSelectors";
 
 import {walletActions} from "../../Core/actions";
 
 import {Button, Icon, Page, PageHeading} from "../../Elements";
+import {AddWalletMethodPicker, ProjectContentLoader} from "../../Components";
 
 class ProjectAddWalletPage extends Component {
+    state = {
+        currentMethod: 'address',
+    };
+
+    componentDidMount() {
+        const {project, walletActions, walletsLoaded} = this.props;
+
+        if (!walletsLoaded) {
+            walletActions.fetchWalletsForProject(project);
+        }
+    }
+
+    setCurrentType = (type) => {
+        this.setState({currentMethod: type,});
+    };
+
     render() {
-        const {project} = this.props;
+        const {project, walletsLoaded} = this.props;
+        const {currentMethod} = this.state;
 
         return (
             <Page>
@@ -22,6 +41,12 @@ class ProjectAddWalletPage extends Component {
                     </Button>
                     <h1>Add Wallet</h1>
                 </PageHeading>
+                {!walletsLoaded && <ProjectContentLoader text="Fetching required data"/>}
+                {walletsLoaded && <Fragment>
+                    <AddWalletMethodPicker onSelect={this.setCurrentType} currentActive={currentMethod}/>
+                    {currentMethod === 'address' && <div>address</div>}
+                    {currentMethod === 'csv' && <div>csv</div>}
+                </Fragment>}
             </Page>
         );
     }
@@ -34,6 +59,7 @@ const mapStateToProps = (state, ownProps) => {
 
     return {
         project,
+        walletsLoaded: areProjectWalletsLoaded(state, project.id),
     };
 };
 
--- src/Pages/Project/ProjectWalletsPage.js
+++ src/Pages/Project/ProjectWalletsPage.js
@@ -1,6 +1,8 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
+
+import {CollaboratorPermissionTypes, ProjectTypes} from "../../Common/constants";
 
 import {
     areProjectWalletsLoaded,
@@ -11,10 +13,14 @@ import {getWalletsForProject} from "../../Common/Selectors/WalletSelectors";
 
 import {walletActions} from "../../Core/actions";
 
-import {Page, PageHeading} from "../../Elements";
-import {ProjectContentLoader, ProjectWalletsList} from "../../Components";
+import {Button, Page, PageHeading} from "../../Elements";
+import {ExampleProjectInfoModal, PermissionControl, ProjectContentLoader, ProjectWalletsList} from "../../Components";
 
 class ProjectWalletsPage extends Component {
+    state = {
+        createProjectModalOpen: false,
+    };
+
     componentDidMount() {
         const {project, walletActions, walletsLoaded} = this.props;
 
@@ -23,13 +29,42 @@ class ProjectWalletsPage extends Component {
         }
     }
 
+    handleOpenExampleProjectInfoModal = () => {
+        this.setState({
+            createProjectModalOpen: true,
+        });
+    };
+
+    handleCloseExampleProjectInfoModal = () => {
+        this.setState({
+            createProjectModalOpen: false,
+        });
+    };
+
     render() {
         const {projectWallets, wallets, walletsLoaded, project} = this.props;
+        const {createProjectModalOpen} = this.state;
 
         return (
             <Page>
                 <PageHeading>
                     <h1>Wallets</h1>
+                    <div className="MarginLeftAuto">
+                        {walletsLoaded && project.type !== ProjectTypes.DEMO &&
+                        <PermissionControl project={project}
+                                           requiredPermission={CollaboratorPermissionTypes.ADD_CONTRACT}>
+                            <Button to={`${project.getUrlBase()}/wallets/add`}>
+                                <span>Add Wallet</span>
+                            </Button>
+                        </PermissionControl>}
+                        {walletsLoaded && project.type === ProjectTypes.DEMO && <Fragment>
+                            <Button onClick={this.handleOpenExampleProjectInfoModal}>
+                                <span>Add Wallet</span>
+                            </Button>
+                            <ExampleProjectInfoModal header="Example Project" description="This is just an example project to illustrate what the platform can do. If you wish to add a wallet first create a project."
+                                                     onClose={this.handleCloseExampleProjectInfoModal} open={createProjectModalOpen}/>
+                        </Fragment>}
+                    </div>
                 </PageHeading>
                 {!walletsLoaded && <ProjectContentLoader text="Fetching project wallets..."/>}
                 {walletsLoaded && <ProjectWalletsList project={project} projectWallets={projectWallets} wallets={wallets}/>}
