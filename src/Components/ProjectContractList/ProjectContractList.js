010f6a94ea211e840e6b4f028fb8e3e84116045f
Miljan Tekic <miljan.tekic@gmail.com>
2019-11-22 13:19:58 +0100

    Contract list grouping by network

--- src/Components/ProjectContractList/ProjectContractList.js
+++ src/Components/ProjectContractList/ProjectContractList.js
@@ -2,11 +2,12 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Blockies from "react-blockies";
 import {Link} from "react-router-dom";
+import _ from 'lodash';
 
 import {Project, ProjectContract} from "../../Core/models";
 // import {NetworkTypes} from "../../Common/constants";
 
-import {Card, Icon, Input} from "../../Elements";
+import {Card, Icon, Tag, Input} from "../../Elements";
 import {
     // ContractAddressColumn,
     // ContractDeployedAtColumn,
@@ -18,6 +19,7 @@ import {
 
 import './ProjectContractList.scss';
 import {generateShortAddress} from "../../Utils/AddressFormatter";
+import {getLabelForNetwork} from "../../Utils/NetworkHelpers";
 
 // const projectContractsTableConfiguration = [
 //     {
@@ -75,37 +77,35 @@ import {generateShortAddress} from "../../Utils/AddressFormatter";
 const ProjectContractListItem = ({projectContract}) => {
     const mainRevision = projectContract.getMainRevision();
 
-    return <Link to={projectContract.getUrl()} key={projectContract.id} className="ProjectContractList__Item">
+    return <Link to={projectContract.getUrl()} className="ProjectContractList__Item">
         <Card clickable>
-            <div className="DisplayFlex AlignItemsCenter">
-                <NetworkTag size="small" network={projectContract.network}/>
-                <div className="MarginLeft2">
-                    <div className="SemiBoldText MarginBottom1">{projectContract.name}</div>
-                    <div className="LinkText MonospaceFont">{generateShortAddress(mainRevision.address, 12, 6)}</div>
-                </div>
+            <div className="ProjectContractList__Item__ContractInfo">
+                <div className="SemiBoldText MarginBottom1 ProjectContractList__Item__ContractName">{projectContract.name}</div>
+                <div className="LinkText MonospaceFont ProjectContractList__Item__Address">{generateShortAddress(mainRevision.address, 12, 6)}</div>
             </div>
             <div className="MarginTop2">
                 {projectContract.revisions.map(revision => <div key={revision.id} className="DisplayFlex AlignItemsCenter MarginBottom1">
                     <Blockies size={8} scale={3} className="BorderRadius1 MarginRight2" seed={revision.id}/>
                     <div className="LinkText MonospaceFont">{generateShortAddress(revision.address, 12, 6)}</div>
+                    {mainRevision.address === revision.address && projectContract.revisions.length > 1 && <Tag className="MarginLeftAuto" size="small" color="success-outline">Active</Tag>}
                 </div>)}
             </div>
             <div className="ProjectContractList__Item__AddedByWrapper">
                 <div className="DisplayFlex AlignItemsCenter">
-                    <div></div>
-                    <div className="MarginLeft2">
-                        <div>Miljan Tekic</div>
-                        <div>Pushed 5 hours ago</div>
+                    <img src={`https://www.gravatar.com/avatar/testpusher?s=32&d=identicon`} alt="User Avatar" className="ProjectContractList__Item__AddedByWrapper__Image"/>
+                    <div className="MarginLeft1">
+                        <div className="SemiBoldText">Miljan Tekic</div>
+                        <div className="MutedText FontSize2">Pushed 5 hours ago</div>
                     </div>
                     <div className="MarginLeftAuto TextAlignRight">
                         {mainRevision.tags.length > 0 && <div>
                             <Icon icon="tag" className="MarginRight1"/>
                             <span>{mainRevision.getLatestTag().label}</span>
                         </div>}
-                        <div>
+                        <Link>
                             <span>View Push</span>
                             <Icon icon="external-link" className="MarginLeft1"/>
-                        </div>
+                        </Link>
                     </div>
                 </div>
             </div>
@@ -133,6 +133,8 @@ class ProjectContractList extends Component{
         const {projectContracts, project} = this.props;
         const {searchQuery} = this.state;
 
+        const groupedContracts = _.groupBy(projectContracts,'network');
+
         return (
             <div className="ProjectContractList">
                 {projectContracts.length > 12 && <div className="ProjectContractList__FilterWrapper">
@@ -140,15 +142,27 @@ class ProjectContractList extends Component{
                         <Input icon="filter" autoComplete="off" label="Filter contracts" field="searchQuery"  value={searchQuery}/>
                     </div>
                 </div>}
-                <div className="ProjectContractList__ItemsWrapper">
-                    {projectContracts.map(projectContract => <ProjectContractListItem key={projectContract.id} projectContract={projectContract}/>)}
-                    <Link to={`/${project.owner}/${project.slug}/contracts/add`} className="ProjectContractList__Item ProjectContractList__Item--AddContract">
-                        <Card clickable>
-                            <Icon icon="plus"/>
-                            <div>Add</div>
-                        </Card>
-                    </Link>
-                </div>
+                {Object.keys(groupedContracts).map(network => <div key={network}>
+                    <div className="ProjectContractList__NetworkHeading">
+                        <div>
+                            <NetworkTag network={network}/>
+                            <span className="SemiBoldText FontSize4 MarginLeft2">{getLabelForNetwork(network)}</span>
+                            <span className="MarginLeft1 MutedText"><Icon icon="file-text"/> {groupedContracts[network].length} Contracts</span>
+                        </div>
+                        <div className="MarginLeftAuto Padding1">
+                            <Icon icon="chevron-down"/>
+                        </div>
+                    </div>
+                    <div className="ProjectContractList__ItemsWrapper">
+                        {groupedContracts[network].map(projectContract => <ProjectContractListItem key={projectContract.id} projectContract={projectContract}/>)}
+                        <Link to={`/${project.owner}/${project.slug}/contracts/add`} className="ProjectContractList__Item ProjectContractList__Item--AddContract">
+                            <Card className="ProjectContractList__Item__Card">
+                                <Icon icon="plus" className="FontSize4 MarginBottom4"/>
+                                <div>Add Contract</div>
+                            </Card>
+                        </Link>
+                    </div>
+                </div>)}
             </div>
         )
     }
