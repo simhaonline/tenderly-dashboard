2ec339f38fec4c6b51aee6b1efd576587cd476a2
Miljan Tekic <miljan.tekic@gmail.com>
2019-11-28 14:54:03 +0100

    Connected to redux and fetched contracts if not fetched

--- src/Pages/Project/ProjectSimulatorPage.js
+++ src/Pages/Project/ProjectSimulatorPage.js
@@ -1,9 +1,23 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
+
+import {areProjectContractsLoaded, getProjectBySlugAndUsername} from "../../Common/Selectors/ProjectSelectors";
+import {getContractsForProject} from "../../Common/Selectors/ContractSelectors";
+
+import {contractActions} from "../../Core/actions";
 
 import {Page, PageHeading} from "../../Elements";
 
 class ProjectSimulatorPage extends Component {
+    async componentDidMount() {
+        const {actions, project, contractsLoaded} = this.props;
+
+        if (!contractsLoaded) {
+            await actions.fetchContractsForProject(project);
+        }
+    }
+
     render() {
         return (
             <Page>
@@ -15,12 +29,22 @@ class ProjectSimulatorPage extends Component {
     }
 }
 
-const mapStateToProps = (state) => {
-    return {};
+const mapStateToProps = (state, ownProps) => {
+    const {match: {params: {slug, username}}} = ownProps;
+
+    const project = getProjectBySlugAndUsername(state, slug, username);
+
+    return {
+        project,
+        contracts: getContractsForProject(state, project.id),
+        contractsLoaded: areProjectContractsLoaded(state, project.id),
+    }
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {};
+    return {
+        actions: bindActionCreators(contractActions, dispatch),
+    }
 };
 
 export default connect(
