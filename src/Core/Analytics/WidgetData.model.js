7fe81549fa7f832581ef7c7ace151c66391de4bd
vvk <vvk@tenderly.dev>
2020-01-09 14:43:56 +0100

    parse widgetdata for custom widgets

--- src/Core/Analytics/WidgetData.model.js
+++ src/Core/Analytics/WidgetData.model.js
@@ -1,3 +1,5 @@
+import chroma from "chroma-js";
+
 class WidgetData {
 
     constructor(data) {
@@ -6,10 +8,29 @@ class WidgetData {
     }
 
     static buildFromResponse(response) {
-        console.log(response)
+        const data = [];
+        const dataPoints = [];
+
+
+        Object.values(response).forEach(widgetResponse => {
+            const legendItems = widgetResponse.legend && widgetResponse.legend.items ? Object.keys(widgetResponse.legend.items) : [];
+            const colorScale = chroma.scale(['#0069E0', '#ADD3FF']).correctLightness();
+
+            if (widgetResponse.data) {
+                data.push(...widgetResponse.data)
+            }
+
+            dataPoints.push(...legendItems.map((itemKey, index) => ({
+                key: itemKey,
+                name: itemKey,
+                color: colorScale(1 / (Math.max(legendItems.length - 1, 1)) * index).hex(),
+                meta: widgetResponse.legend.items[itemKey].labels,
+            })))
+        });
+
         return new WidgetData({
-            data: [],
-            dataPoints: []
+            data,
+            dataPoints
         })
 
     }
