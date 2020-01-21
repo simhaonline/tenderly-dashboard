e443e4f6c1e1228ffbba5fad3ef6548f754c7956
vvkio <io@vvk.io>
2020-01-21 14:44:15 +0100

    Initial styling for grandfather info

--- src/Pages/General/SessionResolutionPage.js
+++ src/Pages/General/SessionResolutionPage.js
@@ -6,7 +6,9 @@ import {bindActionCreators} from "redux";
 import {Plan, User} from "../../Core/models";
 import {billingActions} from "../../Core/actions";
 
-import {Button, LinkButton, Page} from "../../Elements";
+import {Button, Icon, LinkButton, Page} from "../../Elements";
+import GiftIcon from "./giftbox.svg";
+import './SessionResolutionPage.scss';
 
 class SessionResolutionPage extends Component {
     acceptGrandfatherPlan = async () => {
@@ -27,10 +29,40 @@ class SessionResolutionPage extends Component {
 
         return (
             <Page wholeScreenPage>
-                <div className="DisplayFlex FlexDirectionColumn AlignItemsCenter JustifyContentCenter">
-
-                    <Button className="MarginBottom2" onClick={this.acceptGrandfatherPlan} color="secondary">Accept Grandfathering</Button>
-                    <LinkButton>Decline, downgrade to the Free Version</LinkButton>
+                <div className="GrandFatherInfo">
+                    <img src={GiftIcon} className='GrandFatherInfo__Image' alt=""/>
+                    <h1 className='GrandFatherInfo__Headline'>Tenderly is out of Beta!</h1>
+                    <p className='GrandFatherInfo__Description'>To thank you for your support, we have upgraded your account to the Pro plan for the next 90 days.</p>
+                     <div>
+                         <div className='GrandFatherInfo__ProFeaturesWrapper'>
+                             <div className='GrandFatherInfo__ProFeaturesListItem'>
+                                 <Icon className='GrandFatherInfo__ProFeaturesListIcon' icon='organization'/>Multiple
+                                 Projects
+                             </div>
+                             <div className='GrandFatherInfo__ProFeaturesListItem'>
+                                 <Icon className='GrandFatherInfo__ProFeaturesListIcon' icon='users'/>Collaborators
+                             </div>
+                             <div className='GrandFatherInfo__ProFeaturesListItem'>
+                                 <Icon className='GrandFatherInfo__ProFeaturesListIcon' icon='bar-chart-2'/>Analytics
+                             </div>
+                             <div className='GrandFatherInfo__ProFeaturesListItem'>
+                                 <Icon className='GrandFatherInfo__ProFeaturesListIcon' icon='bell'/> Real-time
+                                 Alerting
+                             </div>
+                             <div className='GrandFatherInfo__ProFeaturesListItem'>
+                                 <Icon className='GrandFatherInfo__ProFeaturesListIcon' icon='filter'/>Transaction
+                                 Filtering
+                             </div>
+                             <div className='GrandFatherInfo__ProFeaturesListItem'>
+                                 <Icon className='GrandFatherInfo__ProFeaturesListIcon' icon='layers'/>Private Network
+                                 Support
+                             </div>
+                         </div>
+                     </div>
+                    <Button className="MarginBottom2" size='large' onClick={this.acceptGrandfatherPlan} color="secondary">
+                        <span>Continue</span>
+                        <Icon icon='arrow-right'/>
+                    </Button>
                 </div>
             </Page>
         );
