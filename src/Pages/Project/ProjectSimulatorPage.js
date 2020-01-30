74dd968554adf963377a0b883ff2a37678ffd3c0
vvkio <io@vvk.io>
2020-01-30 11:15:30 +0100

    Empty state for simulator

--- src/Pages/Project/ProjectSimulatorPage.js
+++ src/Pages/Project/ProjectSimulatorPage.js
@@ -1,15 +1,28 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {connect} from 'react-redux';
-import {Page} from "../../Elements";
+import {Page, PanelContent} from "../../Elements";
 import PageHeading from "../../Elements/Page/PageHeading";
 import {areProjectContractsLoaded, getProjectBySlugAndUsername} from "../../Common/Selectors/ProjectSelectors";
 import {getContractsForProject} from "../../Common/Selectors/ContractSelectors";
 import Button from "../../Elements/Button/Button";
+import {EmptyState, ProjectContentLoader} from "../../Components";
+import Panel from "../../Elements/Panel/Panel";
 
 class ProjectSimulatorPage extends Component {
+    state = {
+        transactions: [],
+        loaded: false,
+
+    };
+    componentDidMount() {
+        this.setState({
+            loaded: true,
+        })
+    };
 
     render() {
         const {project} = this.props;
+        const {loaded, transactions} = this.state;
 
         return (
             <Page>
@@ -21,6 +34,15 @@ class ProjectSimulatorPage extends Component {
                         </Button>
                     </div>
                 </PageHeading>
+                {!loaded && <ProjectContentLoader text="Loading simulated transactions..."/>}
+                {loaded && <Fragment>
+                    {transactions.length===0 &&<Panel><PanelContent>
+                        <EmptyState icon="airplay" title="No Transactions" description="You have not performed any simulated transactions yet"
+                                    renderActions={()=><Button to={`${project.getUrlBase()}/simulator/new`} color="secondary">
+                                        <span>Run Simulation</span>
+                                    </Button>}/>
+                    </PanelContent></Panel>}
+                </Fragment>}
             </Page>
         );
     }
